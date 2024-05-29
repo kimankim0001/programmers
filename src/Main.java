8cd2ed9a652371d324b847c4caa2968864aafe23
kinam <maltize9292@gmail.com>
2024-05-29 15:12:56 +0900

    24.05-29 문자열 다루기 기본

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        CodeKata ck = new CodeKata();
+        System.out.println(ck.codekata("A123"));
+        System.out.println(ck.codekata("1234"));
+        System.out.println(ck.codekata("123456"));
+        System.out.println(ck.codekata("1234567"));
+        System.out.println(ck.codekata("12345c"));
+    }
+}
+// 문제 링크
+// https://school.programmers.co.kr/learn/courses/30/lessons/12918
