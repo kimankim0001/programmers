1db927d2e206e66727b26610bceb99ed41fd83a6
kinam <maltize9292@gmail.com>
2024-06-07 09:25:20 +0900

    24.06-07 문자열 내 마음대로 정렬하기

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,14 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String a = "one4seveneight";
-        String b = "23four5six7";
-        String c = "2three45sixseven";
-        String d = "123";
-        System.out.println(solution.solution(a));
-        System.out.println(solution.solution(b));
-        System.out.println(solution.solution(c));
-        System.out.println(solution.solution(d));
-
-
-
-
-
-
+        String[] a = {"sun", "bed", "car"};
+        String[] b = {"abce", "abcd", "cdx"};
+        int a1 = 1;
+        System.out.println(Arrays.toString(solution.solution(a,a1)));
+        System.out.println(Arrays.toString(solution.solution(b,2)));
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/81301
+// https://school.programmers.co.kr/learn/courses/30/lessons/12915
--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,15 @@
+import java.util.*;
+
 class Solution {
-    public int solution(String s) {
-        // 배열에 영단어를 담고
-        // 문자열 안에서 배열 안의 영단어랑 일치하는 걸 찾아주는데
-        // 이걸 반복문으로 돌려주고
-        // 일치하는걸 배열의 인덱스 값으로 바꿔주는 함수를 찾아봐야겠네
-        // String a = "무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세 ";
-        // replace([기존문자],[바꿀문자])
-        // a= a.replace("대한", "민국");
-        // System.out.println(a);
-        //결과값 : 무궁화 삼천리 화려강산 민국사람 민국으로 길이 보전하세
-        // s.replace() 이걸 사용하면 되겠다
-        String[] arr = {"zero","one","two","three","four","five","six","seven","eight","nine"};
-        for (int i = 0; i < 10; i++){
-            s = s.replace(arr[i],String.valueOf(i));
-        }
-        int answer = Integer.parseInt(s);
+    public String[] solution(String[] strings, int n) {
+        // 일단 문자열을 먼저 정렬해주고
+        // 그 다음에 조건에 맞게 정렬하면 n 번째 인덱스가 같은걸 해결 가능
+        // 최근에 다시 학습한 람다식을 사용스
+        // 오류 확인, 유틸 임포트 안했음 습관화해야겠다
+        String[] answer = {};
+        Arrays.sort(strings);
+        Arrays.sort(strings, Comparator.comparing((str) -> str.substring(n, n+1)));
+        answer = strings;
         return answer;
     }
 }
