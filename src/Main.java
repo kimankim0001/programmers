5a5b98d565ceeeb1c2edb67eb476711a55e9607e
kinam <maltize9292@gmail.com>
2024-06-09 17:41:35 +0900

    24.06-09 두 개 뽑아서 더하기

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,13 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int [] a = {1, 5, 2, 6, 3, 7, 4};
-        int [][] b = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
-        System.out.println(Arrays.toString(solution.solution(a, b)));
+        int [] a = {2,1,3,4,1};
+        int [] b = {5,0,2,7};
+
+        System.out.println(Arrays.toString(solution.solution(a)));
+        System.out.println(Arrays.toString(solution.solution(b)));
 
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/42748
+// https://school.programmers.co.kr/learn/courses/30/lessons/68644
--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,21 @@
 import java.util.*;
-
 class Solution {
-    public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-
-        for(int i = 0; i < commands.length; i++){
-            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
-            Arrays.sort(temp);
-            answer[i] = temp[commands[i][2] - 1];
+    public int[] solution(int[] numbers) {
+        // 중복값을 허용하지 않는 set 을 사용해서 모든 경우의 수를 다 더해보고
+        // set 에 넣어주고 answer 배열에 하나씩 넣우주고 리턴
+        Set<Integer> set = new HashSet<>();
+        for (int i=0; i<numbers.length; i++){
+            for (int j = i+1; j<numbers.length; j++){
+                int sum = numbers[i]+numbers[j];
+                set.add(sum);
+            }
         }
+        int[] answer = new int[set.size()];
+        int idx = 0;
+        for (int num : set){
+            answer[idx++] = num;
+        }
+        Arrays.sort(answer);
         return answer;
     }
 }
