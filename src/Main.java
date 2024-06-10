9f99dee7a5bd677ca535b126b60ac511f45a98ab
kinam <maltize9292@gmail.com>
2024-06-10 09:38:24 +0900

    24.06-10 가장 가까운 같은 글자

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,12 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int [] a = {2,1,3,4,1};
-        int [] b = {5,0,2,7};
-
+        String a = "banana";
+        String b = "foobar";
         System.out.println(Arrays.toString(solution.solution(a)));
         System.out.println(Arrays.toString(solution.solution(b)));
 
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/68644
+// https://school.programmers.co.kr/learn/courses/30/lessons/142086
--- src/Solution.java
+++ src/Solution.java
@@ -1,21 +1,19 @@
-import java.util.*;
 class Solution {
-    public int[] solution(int[] numbers) {
-        // 중복값을 허용하지 않는 set 을 사용해서 모든 경우의 수를 다 더해보고
-        // set 에 넣어주고 answer 배열에 하나씩 넣우주고 리턴
-        Set<Integer> set = new HashSet<>();
-        for (int i=0; i<numbers.length; i++){
-            for (int j = i+1; j<numbers.length; j++){
-                int sum = numbers[i]+numbers[j];
-                set.add(sum);
+    public int[] solution(String s) {
+        // answer 길이 s.length() 와 동일하게 선언해주고
+        // 이중 for 문을 돌려 같은 글자 answer[i] 에 저장
+        // for 문 끝나고 answer[i] 가 0 이라면 같은 글자가 없으니까 -1 리턴
+        int[] answer = new int[s.length()];
+        for (int i = 0; i < s.length(); i++){
+            for (int j = i; j >= 0; j--){
+                if (s.charAt(i) == s.charAt(j) && answer[i] == 0){
+                    answer[i] = i - j;
+                }
+            }
+            if (answer[i] == 0){
+                answer[i] = -1;
             }
         }
-        int[] answer = new int[set.size()];
-        int idx = 0;
-        for (int num : set){
-            answer[idx++] = num;
-        }
-        Arrays.sort(answer);
         return answer;
     }
 }
