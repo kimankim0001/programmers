1c31f24f2ebb3990583d00dcdc8f28f7d2ec64fc
kinam <maltize9292@gmail.com>
2024-06-11 09:56:43 +0900

    24.06-11 푸드 파이트 대회

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
-import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String a = "banana";
-        String b = "foobar";
-        System.out.println(Arrays.toString(solution.solution(a)));
-        System.out.println(Arrays.toString(solution.solution(b)));
+        int[] a = {1, 3, 4, 6};
+        int[] b = {1, 7, 1, 2};
+
+        System.out.println(solution.solution(a));
+        System.out.println(solution.solution(b));
 
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/142086
+// https://school.programmers.co.kr/learn/courses/30/lessons/134240
--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,13 @@
 class Solution {
-    public int[] solution(String s) {
-        // answer 길이 s.length() 와 동일하게 선언해주고
-        // 이중 for 문을 돌려 같은 글자 answer[i] 에 저장
-        // for 문 끝나고 answer[i] 가 0 이라면 같은 글자가 없으니까 -1 리턴
-        int[] answer = new int[s.length()];
-        for (int i = 0; i < s.length(); i++){
-            for (int j = i; j >= 0; j--){
-                if (s.charAt(i) == s.charAt(j) && answer[i] == 0){
-                    answer[i] = i - j;
-                }
-            }
-            if (answer[i] == 0){
-                answer[i] = -1;
-            }
+    public String solution(int[] food) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < food.length; i++) {
+            int count = food[i] / 2;
+            sb.append(String.valueOf(i).repeat(count));
         }
+        String answer = sb + "0";
+        answer += sb.reverse();
         return answer;
     }
 }
