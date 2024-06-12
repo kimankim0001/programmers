04ff29339cb5ff85aa978990f59de2ea67eba36b
kinam <maltize9292@gmail.com>
2024-06-12 09:36:37 +0900

    24.06-12 콜라 문제

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,10 @@
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] a = {1, 3, 4, 6};
-        int[] b = {1, 7, 1, 2};
-
-        System.out.println(solution.solution(a));
-        System.out.println(solution.solution(b));
+        System.out.println(solution.solution(2,1,20));
+        System.out.println(solution.solution(3,1,20));
 
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/134240
+// https://school.programmers.co.kr/learn/courses/30/lessons/132267
--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,17 @@
 class Solution {
-    public String solution(int[] food) {
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 1; i < food.length; i++) {
-            int count = food[i] / 2;
-            sb.append(String.valueOf(i).repeat(count));
+    public int solution(int a, int b, int n) {
+        // a 개를 주면 b 개를 돌려주는데에 n 개를 가져갔을때의 리턴
+        // answer += n / a * b 상빈이가 받는 새콜라
+        // n = (n / a * b) + (n % a) 상빈이가 가지고 있는 콜라
+        // n >= a 콜라를 돌려 받을 수 있는 최소 조건
+        int answer = 0;
+        // n >= a 콜라를 돌려 받을 수 있는 최소 조건
+        while(n>=a){
+            // answer += n / a * b 상빈이가 받는 새콜라
+            answer += n / a * b;
+            // n = (n / a * b) + (n % a) 상빈이가 가지고 있는 콜라
+            n = (n / a * b) + (n % a);
         }
-        String answer = sb + "0";
-        answer += sb.reverse();
         return answer;
     }
 }
