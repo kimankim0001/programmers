8f999746ca48a9fb7ee2021eea6866306f4d7e4d
kinam <maltize9292@gmail.com>
2024-06-13 10:35:28 +0900

    24.06-13 명예의 전당

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
-
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(2,1,20));
-        System.out.println(solution.solution(3,1,20));
+        int[] a = {10, 100, 20, 150, 1, 100, 200};
+        int[] b = {0, 300, 40, 300, 20, 70, 150, 50, 500, 1000};
+
+        System.out.println(Arrays.toString(solution.solution(3,a)));
+        System.out.println(Arrays.toString(solution.solution(4,b)));
+
+
 
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/132267
+// https://school.programmers.co.kr/learn/courses/30/lessons/138477
