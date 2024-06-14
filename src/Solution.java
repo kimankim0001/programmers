5511ba4608b0595476e2605d12ace8b73232b63e
kinam <maltize9292@gmail.com>
2024-06-14 09:34:52 +0900

    24.06-14 2016년

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,24 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.time.LocalDate;
 
 class Solution {
-    public int[] solution(int k, int[] score) {
-        int[] answer = new int[score.length];
-        List<Integer> rank = new ArrayList<>();
-        for (int i = 0; i < score.length; i++) {
-            rank.add(score[i]);
-            if (rank.size() > k) {
-                rank.remove(Collections.min(rank));
-            }
-            answer[i] = Collections.min(rank);
-        }
+    public String solution(int a, int b) {
+        // 달력, 날짜 나타낼 수 있는 함수 찾기
+        // 과제를 하면서 썼던 LocalDate 를 쓸 수 있지 않을까
+        // import java.time.LocalDate; 임포트
+        // of() 메소드는 전달된 인수를 가지고 특정 날짜를 표현하는 새로운 객체를 생성하여 반환
+        // LocalDate date = LocalDate.of(2016, a, b);
+        // 반환된 날짜를 요일로 나타내는 방법 찾기
+        // answer = date.getDayOfWeek();
+        // import java.time.DayOfWeek; 임포트
+        // String 타입으로 반환해줘야했다
+        // answer = String.valueOf(date.getDayOfWeek());
+        // 3글자로 반환
+        // answer = String.valueOf(date.getDayOfWeek()).substring(0,3);
+
+        String answer = "";
+        LocalDate date = LocalDate.of(2016, a, b);
+        answer = String.valueOf(date.getDayOfWeek()).substring(0,3);
+
         return answer;
     }
 }
