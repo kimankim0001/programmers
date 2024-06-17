7d53234e95f81deef4267d9875ac57e53a6cbcb9
kinam <maltize9292@gmail.com>
2024-06-17 16:08:35 +0900

    24.06-17 모의고사

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        int[] a = {1,2,3,1,2,3,1};
-        int[] b = {4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2};
+        int[] a = {1,2,3,4,5};
+        int[] b = {1,3,2,4,2};
 
-        System.out.println(solution.solution(3,4,a));
-        System.out.println(solution.solution(4,3,b));
-
-
-
-
+        System.out.println(Arrays.toString(solution.solution(a)));
+        System.out.println(Arrays.toString(solution.solution(b)));
 
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/135808
+// https://school.programmers.co.kr/learn/courses/30/lessons/42840
--- src/Solution.java
+++ src/Solution.java
@@ -1,17 +1,49 @@
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 class Solution {
-    public int solution(int k, int m, int[] score) {
-        // 최종 함수 : k * m
-        // score 를 내림차순으로 정렬
-        // for (int i = 1; i*m-1 < score.length; i++)
-        // answer += score[i*m] * m;
-        Integer[] tmp = Arrays.stream(score).boxed().toArray(Integer[]::new);
-        Arrays.sort(tmp, Comparator.reverseOrder());
-        int answer = 0;
-        for (int i = 1; i*m-1 < tmp.length; i++){
-            answer += tmp[i*m-1] * m;
+    public int[] solution(int[] answers) {
+        // 수포자 하나씩 배열에 패턴 담기
+        // 수포자들의 점수 배열에 담기
+        // 반복문으로 ansqwers 인덱스와 수포자들 인덱스값 같으면 점수 배열값 증가
+        // 점수 배열 최대 값 찾기
+        // 최고 점수 수포자 담기
+        // 배열로 리턴
+
+        // 수포자 하나씩 배열에 패턴 담기
+        int[] supoza1 = {1,2,3,4,5};
+        int[] supoza2 = {2,1,2,3,2,4,2,5};
+        int[] supoza3 = {3,3,1,1,2,2,4,4,5,5};
+        // 수포자들의 점수 배열에 담기
+        int[] scores = {0,0,0};
+
+        // 반복문으로 ansqwers 인덱스와 수포자들 인덱스값 같으면 점수 배열값 증가
+        for(int i = 0; i < answers.length; i++){
+            if(answers[i] == supoza1[i%5]) {
+                scores[0]++;
+            }
+            if(answers[i] == supoza2[i%8]) {
+                scores[1]++;
+            }
+            if(answers[i] == supoza3[i%10]) {
+                scores[2]++;
+            }
+        }
+
+        // 점수 배열 최대 값 찾기
+        int maxScore = Math.max(scores[0], Math.max(scores[1], scores[2]));
+
+        // 최고 점수 수포자 담기
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < 3; i++){
+            if(maxScore == scores[i]) {
+                list.add(i+1);
+            }
+        }
+
+        // 배열로 리턴
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
         }
 
         return answer;
