661d3b80f35f1b8d71c6030428db4addaed8ad09
kinam <maltize9292@gmail.com>
2024-06-18 10:03:58 +0900

    24.06-18 소수 만들기

--- src/Solution.java
+++ src/Solution.java
@@ -1,51 +1,32 @@
-import java.util.*;
-
 class Solution {
-    public int[] solution(int[] answers) {
-        // 수포자 하나씩 배열에 패턴 담기
-        // 수포자들의 점수 배열에 담기
-        // 반복문으로 ansqwers 인덱스와 수포자들 인덱스값 같으면 점수 배열값 증가
-        // 점수 배열 최대 값 찾기
-        // 최고 점수 수포자 담기
-        // 배열로 리턴
-
-        // 수포자 하나씩 배열에 패턴 담기
-        int[] supoza1 = {1,2,3,4,5};
-        int[] supoza2 = {2,1,2,3,2,4,2,5};
-        int[] supoza3 = {3,3,1,1,2,2,4,4,5,5};
-        // 수포자들의 점수 배열에 담기
-        int[] scores = {0,0,0};
-
-        // 반복문으로 ansqwers 인덱스와 수포자들 인덱스값 같으면 점수 배열값 증가
-        for(int i = 0; i < answers.length; i++){
-            if(answers[i] == supoza1[i%5]) {
-                scores[0]++;
-            }
-            if(answers[i] == supoza2[i%8]) {
-                scores[1]++;
-            }
-            if(answers[i] == supoza3[i%10]) {
-                scores[2]++;
+    public int solution(int[] nums) {
+        // 3개의 수를 더하기
+        // 소수이면 answer++;
+        // 소수판별기
+        int answer = 0;
+        int sum = 0;
+        // 3개의 수를 더하기
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    sum = nums[i] + nums[j] + nums[k];
+                    // 소수이면 answer++;
+                    if (isSosu(sum)) {
+                        answer++;
+                    }
+                }
             }
         }
-
-        // 점수 배열 최대 값 찾기
-        int maxScore = Math.max(scores[0], Math.max(scores[1], scores[2]));
-
-        // 최고 점수 수포자 담기
-        List<Integer> list = new ArrayList<>();
-        for(int i = 0; i < 3; i++){
-            if(maxScore == scores[i]) {
-                list.add(i+1);
-            }
-        }
-
-        // 배열로 리턴
-        int[] answer = new int[list.size()];
-        for(int i = 0; i < list.size(); i++){
-            answer[i] = list.get(i);
-        }
-
         return answer;
+    }
+
+    // 소수판별기
+    private boolean isSosu(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
