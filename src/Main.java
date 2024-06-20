7c8e79c88888e13cfc68bd368c3163e9e8fc745a
kinam <maltize9292@gmail.com>
2024-06-20 10:00:28 +0900

    24.06-20 기사단원의 무기

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        int[] a = {2, 3, 6};
-        int[] b = {1, 3};
-        int[] c = {1, 2, 3, 4};
-
-        System.out.println(solution.solution(8,4,a));
-        System.out.println(solution.solution(5,4,b));
-        System.out.println(solution.solution(4,1,c));
+        System.out.println(solution.solution(5,3,2));
+        System.out.println(solution.solution(10,3,2));
 
 
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/161989
+// https://school.programmers.co.kr/learn/courses/30/lessons/136798
--- src/Solution.java
+++ src/Solution.java
@@ -1,17 +1,28 @@
 class Solution {
-    public int solution(int n, int m, int[] section) {
-        // 페인트가 칠해진 길이 n
-        // 페인트를 칠하는 롤러의 길이 m
-        // 페인트를 칠해야 하는 1미터 단위 구역 section
+    public int solution(int number, int limit, int power) {
+        // 기사 1~number 넘버링
+        // 기사 번호의 약수 개수 = 공격력
+        // 협약 공격력 limit 넘을경우 협약기관에서 정한 공격력 power
+        // 기사놈들의 총 공격력 구해야한다
+        // 시간 초과 떠서 약수를 구하는 다른 방식 참고하여 리팩토링
 
-        int answer = 1;
-        int index = section[0];
-        for(int i = 1; i < section.length; i++){
-            if(index + m - 1 < section[i]){
-                answer++;
-                index = section[i];
+        int answer = 0;
+        // 기사들의 무기 공격력 구하기
+        for (int i = 1; i <= number; i++){
+            int cnt = 0;
+            for (int j = 1; j*j <= i; j++){
+                if(j*j==i){
+                    cnt++;
+                } else if(i % j == 0){
+                    cnt+=2;
+                }
             }
+            if (cnt > limit) {
+                cnt = power;
+            }
+            answer += cnt;
         }
+
         return answer;
     }
 }
