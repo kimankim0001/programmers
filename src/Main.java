4cc27f22d59c217a540bdc6fe2d07a873a4ce367
kinam <maltize9292@gmail.com>
2024-06-22 12:29:55 +0900

    24.06-22 옹알이(2)

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,13 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        int[] a = {44, 1, 0, 0, 31, 25};
-        int[] a1 = {0, 0, 0, 0, 0, 0};
-        int[] a2 = {45, 4, 35, 20, 3, 9};
+        String[] a = {"aya", "yee", "u", "maa"};
+        String[] b = {"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"};
 
-        int[] b = {31, 10, 45, 1, 6, 19};
-        int[] b1 = {38, 19, 20, 40, 15, 25};
-        int[] b2 = {20, 9, 3, 45, 4, 35};
-
-        System.out.println(Arrays.toString(solution.solution(a,b)));
-        System.out.println(Arrays.toString(solution.solution(a1,b1)));
-        System.out.println(Arrays.toString(solution.solution(a2,b2)));
-
+        System.out.println(solution.solution(a));
+        System.out.println(solution.solution(b));
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/77484#
+// https://school.programmers.co.kr/learn/courses/30/lessons/133499
