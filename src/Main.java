4c07406d560c34aa4a05ccfc1ddfc9c07919cb49
kinam <maltize9292@gmail.com>
2024-06-23 17:45:35 +0900

    24.06-23 숫자 짝꿍

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,25 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        String[] a = {"aya", "yee", "u", "maa"};
-        String[] b = {"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"};
+        String a1 = "100";
+        String a2 = "100";
+        String a3 = "100";
+        String a4 = "12321";
+        String a5 = "5525";
 
-        System.out.println(solution.solution(a));
-        System.out.println(solution.solution(b));
+        String b1 = "2345";
+        String b2 = "203045";
+        String b3 = "123450";
+        String b4 = "42531";
+        String b5 = "1255";
+
+        System.out.println(solution.solution(a1,b1));
+        System.out.println(solution.solution(a2,b2));
+        System.out.println(solution.solution(a3,b3));
+        System.out.println(solution.solution(a4,b4));
+        System.out.println(solution.solution(a5,b5));
+
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/133499
+// https://school.programmers.co.kr/learn/courses/30/lessons/131128
--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,41 @@
 class Solution {
-    public int solution(String[] babbling) {
-        // 전략 :
-        // 1.babbling 문자열에 "aya", "ye", "woo", "ma" 가 있으면 공백으로 만들기
-        // 2.반복되는 발음 "ayaaya", "yeye", "woowoo", "mama" 가 있으면 "X" 로 만들기
-        // 3.공백을 지웠을때 해당 문자열의 길이가 0이면 answer++;
-        // 필요한 함수 :
-        // 1.문자열에 해당되는 모든 값을 변경하는 replaceAll("찾아바꿀문자열", "넣어줄문자열")
-        // 2.공백을 지워주는 trim()
-        // 테스트 :
-        // 전략의 1을 먼저 실행하면 반복되는 발음을 모두 공백으로 바꿔버림
-        // 전략의 2를 먼저 실행하도록 리팩토링
-        int answer = 0;
-        String[] str = {"aya", "ye", "woo", "ma"};
-        String[] repeatStr = {"ayaaya", "yeye", "woowoo", "mama"};
-        for (int i = 0; i < babbling.length; i++) {
-            for (int j = 0; j < str.length; j++) {
-                babbling[i] = babbling[i].replaceAll(repeatStr[j], "X").replaceAll(str[j], " ");
-            }
-            if (babbling[i].trim().length() == 0) {
-                answer++;
+    public String solution(String X, String Y) {
+
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+
+        char[] x = X.toCharArray();
+        char[] y = Y.toCharArray();
+
+        int[] xCheckCnt = new int[10];
+        int[] yCheckCnt = new int[10];
+
+        for(int i = 0; i < x.length; i++) {
+            int tmp = x[i] - '0';
+            xCheckCnt[tmp]++;
+        }
+        for(int i = 0; i < y.length; i++) {
+            int tmp = y[i] - '0';
+            yCheckCnt[tmp]++;
+        }
+
+        for(int i = 9; i >= 0; i--) {
+            int cnt = Math.min(xCheckCnt[i], yCheckCnt[i]);
+            for(int t = 0; t < cnt; t++) {
+                sb.append(i);
             }
         }
+
+        answer = sb.toString();
+
+        if(answer.length() == 0) {
+            answer = "-1";
+        }
+
+        if(answer.charAt(0) == '0') {
+            answer = "0";
+        }
+
         return answer;
     }
 }
