6cf5457631064c4d74d9cc2f4e77c7550f0efddc
kinam <maltize9292@gmail.com>
2024-06-26 09:50:25 +0900

    24.06-26 대충 만든 자판

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,23 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("banana"));
-        System.out.println(solution.solution("abracadabra"));
-        System.out.println(solution.solution("aaabbaccccabba"));
+        String[] k1 = {"ABACD", "BCEFD"};
+        String[] k2 = {"AA"};
+        String[] k3 = {"AGZ", "BSSS"};
+
+        String[] t1 = {"ABCD","AABB"};
+        String[] t2 = {"B"};
+        String[] t3 = {"ASA","BGZ"};
+
+        System.out.println(Arrays.toString(solution.solution(k1,t1)));
+        System.out.println(Arrays.toString(solution.solution(k2,t2)));
+        System.out.println(Arrays.toString(solution.solution(k3,t3)));
+
 
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/140108
+// https://school.programmers.co.kr/learn/courses/30/lessons/160586
--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,48 @@
+import java.util.*;
+
 class Solution {
-    public int solution(String s) {
-        int answer = 1;
+    public int[] solution(String[] keymap, String[] targets) {
+        // 전략 :
+        // 키 밸류 저장하는 HashMap 을 사용
+        // keymap 의 값을 HashMap 에 넣어주자
+        // 이때, 중복되는 키 값은 비교하여 최소값으로 넣어주자
+        // targets 의 값과 keypad 의 값의 중복값을 리턴해주고
+        // 중복값이 없을 때는 -1을 리턴하고 종료
+        // 이때, 앞선 값들이 있더라도 -1을 리턴해야 하므로 boolean 을 사용
+        int[] answer = new int[targets.length];
+        HashMap<Character, Integer> keypad = new HashMap<>();
 
-        // 문자의 갯수
-        int count = 1;
-
-        // 이전 문자
-        char x = s.charAt(0);
-
-        // 문자를 비교 같으면 문자의 갯수 늘려주고 다르면 문자의 갯수 줄여주고
-        // 문자의 갯수 0이되면 조건에 횟수가 같아지는 순간이므로 문자열 분리
-        for (int i = 1; i < s.length(); i++) {
-            if (count == 0) {
-                answer++;
-                x = s.charAt(i);
-            }
-            if (x == s.charAt(i)) {
-                count++;
-            } else {
-                count--;
+        for(int i = 0; i < keymap.length; i++){
+            for(int j = 0; j < keymap[i].length(); j++){
+                char cur = keymap[i].charAt(j);
+                if(keypad.containsKey(cur)){
+                    int idx = keypad.get(cur);
+                    keypad.put(cur, Math.min(idx, j+1));
+                } else{
+                    keypad.put(cur, j+1);
+                }
             }
         }
+
+        for(int i = 0; i < targets.length; i++){
+            String target = targets[i];
+            int count = 0;
+            boolean flag = true;
+            for(char cur : target.toCharArray()){
+                if(keypad.containsKey(cur)){
+                    count += keypad.get(cur);
+                } else{
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag == false){
+                answer[i] = -1;
+            } else {
+                answer[i] = count;
+            }
+        }
+
         return answer;
     }
 }
