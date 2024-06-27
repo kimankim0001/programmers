836caa46576d76fefcf91f0f3b340185f5d11748
kinam <maltize9292@gmail.com>
2024-06-27 11:01:24 +0900

    24.06-27 둘만의 암호

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,12 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        String[] k1 = {"ABACD", "BCEFD"};
-        String[] k2 = {"AA"};
-        String[] k3 = {"AGZ", "BSSS"};
-
-        String[] t1 = {"ABCD","AABB"};
-        String[] t2 = {"B"};
-        String[] t3 = {"ASA","BGZ"};
-
-        System.out.println(Arrays.toString(solution.solution(k1,t1)));
-        System.out.println(Arrays.toString(solution.solution(k2,t2)));
-        System.out.println(Arrays.toString(solution.solution(k3,t3)));
+        System.out.println(solution.solution("aukks","wbqd",5));
+        System.out.println(solution.solution("ilsa","abzq",3));
 
 
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/160586
+// https://school.programmers.co.kr/learn/courses/30/lessons/155652
--- src/Solution.java
+++ src/Solution.java
@@ -1,48 +1,28 @@
-import java.util.*;
-
 class Solution {
-    public int[] solution(String[] keymap, String[] targets) {
+    public String solution(String s, String skip, int index) {
         // 전략 :
-        // 키 밸류 저장하는 HashMap 을 사용
-        // keymap 의 값을 HashMap 에 넣어주자
-        // 이때, 중복되는 키 값은 비교하여 최소값으로 넣어주자
-        // targets 의 값과 keypad 의 값의 중복값을 리턴해주고
-        // 중복값이 없을 때는 -1을 리턴하고 종료
-        // 이때, 앞선 값들이 있더라도 -1을 리턴해야 하므로 boolean 을 사용
-        int[] answer = new int[targets.length];
-        HashMap<Character, Integer> keypad = new HashMap<>();
-
-        for(int i = 0; i < keymap.length; i++){
-            for(int j = 0; j < keymap[i].length(); j++){
-                char cur = keymap[i].charAt(j);
-                if(keypad.containsKey(cur)){
-                    int idx = keypad.get(cur);
-                    keypad.put(cur, Math.min(idx, j+1));
-                } else{
-                    keypad.put(cur, j+1);
+        // 1. s 를 char c 로 하나씩 형변환
+        // 2. index 만큼 c++
+        // 2-1. 여기서 조건은 c > z 다시 a
+        // 2-2. 추가 조건은 skip 과 중복일때 index 한번더 추가
+        // 코드 :
+        // 1. char c = s.charAt()
+        // 2-1. if(c > z) {c = c - 26}
+        // 2-2. if(skip.contains(String.valueOf(c))){i = i-1}
+        String answer = "";
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            for(int j = 0; j < index; j++){
+                c += 1;
+                if(c > 'z'){
+                    c -= 26;
+                }
+                if(skip.contains(String.valueOf(c))){
+                    j--;
                 }
             }
+            answer += c;
         }
-
-        for(int i = 0; i < targets.length; i++){
-            String target = targets[i];
-            int count = 0;
-            boolean flag = true;
-            for(char cur : target.toCharArray()){
-                if(keypad.containsKey(cur)){
-                    count += keypad.get(cur);
-                } else{
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag == false){
-                answer[i] = -1;
-            } else {
-                answer[i] = count;
-            }
-        }
-
         return answer;
     }
 }
