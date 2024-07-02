efd0e24981c88fef7c78048d1d827d94dfa20b04
kinam <maltize9292@gmail.com>
2024-07-02 09:53:27 +0900

    24.07-02 달리기 경주

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        String a1 = "2022.05.19";
-        String a2 = "2020.01.01";
+        String[] a = {"mumu", "soe", "poe", "kai", "mine"};
+        String[] b = {"kai", "kai", "mine", "mine"};
 
-        String[] b1 = {"A 6", "B 12", "C 3"};
-        String[] b2 = {"Z 3", "D 5"};
-
-        String[] c1 = {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
-        String[] c2 = {"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"};
-
-
-        System.out.println(Arrays.toString(solution.solution(a1, b1, c1)));
-        System.out.println(Arrays.toString(solution.solution(a2, b2, c2)));
-
+        System.out.println(Arrays.toString(solution.solution(a, b)));
 
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/150370
+// https://school.programmers.co.kr/learn/courses/30/lessons/178871
--- src/Solution.java
+++ src/Solution.java
@@ -1,33 +1,23 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String today, String[] terms, String[] privacies) {
+    public String[] solution(String[] players, String[] callings) {
+        String[] answer = players;
 
-        ArrayList<Integer> answer = new ArrayList<>();
-        HashMap<String, Integer> map = new HashMap<>();
+        Map<String, Integer> rankMap = new HashMap<>();
 
-        int date = getDate(today);
-
-        for (String s : terms) {
-            String[] term = s.split(" ");
-            map.put(term[0], Integer.parseInt(term[1]));
+        for (int i = 0; i < answer.length; i++) {
+            rankMap.put(answer[i], i);
         }
 
-        for (int i = 0; i < privacies.length; i++) {
-            String[] privacy = privacies[i].split(" ");
-
-            if (getDate(privacy[0]) + (map.get(privacy[1]) * 28) <= date) {
-                answer.add(i + 1);
-            }
+        for (String player : callings) {
+            int rank = rankMap.get(player);
+            String pPlayer = answer[rank-1];
+            answer[rank-1] = player;
+            answer[rank] = pPlayer;
+            rankMap.put(player, rank-1);
+            rankMap.put(pPlayer, rank);
         }
-        return answer.stream().mapToInt(integer -> integer).toArray();
-    }
-
-    private int getDate(String today) {
-        String[] date = today.split("\\.");
-        int year = Integer.parseInt(date[0]);
-        int month = Integer.parseInt(date[1]);
-        int day = Integer.parseInt(date[2]);
-        return (year * 12 * 28) + (month * 28) + day;
+        return answer;
     }
 }
