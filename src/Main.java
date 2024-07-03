b80ef3f28a4dfd0cb2a799709c74607d9038034b
kinam <maltize9292@gmail.com>
2024-07-03 10:11:16 +0900

    24.07-03 공원 산책

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        String[] a = {"mumu", "soe", "poe", "kai", "mine"};
-        String[] b = {"kai", "kai", "mine", "mine"};
+        String[] a1 = {"SOO", "OOO", "OOO"};
+        String[] a2 = {"SOO", "OXX", "OOO"};
+        String[] a3 = {"OSO", "OOO", "OXO", "OOO"};
 
-        System.out.println(Arrays.toString(solution.solution(a, b)));
+        String[] b1 = {"E 2", "S 2", "W 1"};
+        String[] b2 = {"E 2", "S 2", "W 1"};
+        String[] b3 = {"E 2", "S 3", "W 1"};
+
+        System.out.println(Arrays.toString(solution.solution(a1, b1)));
+        System.out.println(Arrays.toString(solution.solution(a2, b2)));
+        System.out.println(Arrays.toString(solution.solution(a3, b3)));
 
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/178871
+// https://school.programmers.co.kr/learn/courses/30/lessons/172928
