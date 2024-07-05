2435991beb761f077a65f33d1d4d24c1970223d6
kinam <maltize9292@gmail.com>
2024-07-05 15:27:22 +0900

    24.07-05 최댓값과 최솟값

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,11 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        String[] a1 = {"muzi", "frodo", "apeach", "neo"};
-        String[] a2 = {"con", "ryan"};
-
-        String[] b1 = {"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"};
-        String[] b2 = {"ryan con", "ryan con", "ryan con", "ryan con"};
-
-        System.out.println(Arrays.toString(solution.solution(a1, b1, 2)));
-        System.out.println(Arrays.toString(solution.solution(a2, b2, 3)));
-
-
+        System.out.println(solution.solution("1 2 3 4"));
+        System.out.println(solution.solution("-1 -2 -3 -4"));
+        System.out.println(solution.solution("-1 -1"));
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/92334
+// https://school.programmers.co.kr/learn/courses/30/lessons/12939
--- src/Solution.java
+++ src/Solution.java
@@ -1,35 +1,34 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-
 class Solution {
-    public int[] solution(String[] idList, String[] report, int k) {
+    public String solution(String s) {
+        // 전략 :
+        // s 에서 하나씩 잘라내서 String 배열에 담기
+        // String 배열과 길이가 같은 int 배열 선언
+        // int 배열에 String 배열값 담기
+        // 최대값과 최소값 구하기
+        String answer = "";
 
-        int[] answer = new int[idList.length];
-        HashMap<String, HashSet<String>> reporterInfoMap = new HashMap<>();
-        HashMap<String, Integer> reportedCountInfoMap = new HashMap<>();
-        HashSet<String> reportSet = new HashSet<>(Arrays.asList(report));
+        String[] strArr = s.split(" ");
 
-        for (String reportInfo : reportSet) {
-            String reporter = reportInfo.split(" ")[0];
-            String reported = reportInfo.split(" ")[1];
-            reporterInfoMap.putIfAbsent(reporter, new HashSet<String>() {{
-                add(reported);
-            }});
-            reporterInfoMap.get(reporter).add(reported);
-            reportedCountInfoMap.put(reported, reportedCountInfoMap.getOrDefault(reported, 0) + 1);
+        int[] intArr = new int[strArr.length];
+
+        for (int i = 0; i < strArr.length; i++) {
+            intArr[i] = Integer.parseInt(strArr[i]);
         }
 
-        for (String reported : reportedCountInfoMap.keySet()) {
-            int reportedCount = reportedCountInfoMap.get(reported);
-            if (reportedCount >= k) {
-                for (int i = 0; i < idList.length; i++) {
-                    if (reporterInfoMap.containsKey(idList[i]) && reporterInfoMap.get(idList[i]).contains(reported)) {
-                        answer[i]++;
-                    }
-                }
+        int max = intArr[0];
+
+        int min = intArr[0];
+
+        for (int i = 0; i < intArr.length; i++) {
+            if (intArr[i] > max) {
+                max = intArr[i];
+            }
+            if (intArr[i] < min) {
+                min = intArr[i];
             }
         }
+        answer = min + " " + max;
+
         return answer;
     }
 }
