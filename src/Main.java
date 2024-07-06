66fbc679907bf57593c2fde08105c26c31bc6dd9
kinam <maltize9292@gmail.com>
2024-07-06 19:26:19 +0900

    24.07-06 JadenCase 문자열 만들기

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("1 2 3 4"));
-        System.out.println(solution.solution("-1 -2 -3 -4"));
-        System.out.println(solution.solution("-1 -1"));
+        System.out.println(solution.solution("3people unFollowed me"));
+        System.out.println(solution.solution("for the last week"));
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/12939
+// https://school.programmers.co.kr/learn/courses/30/lessons/12951
--- src/Solution.java
+++ src/Solution.java
@@ -1,34 +1,22 @@
 class Solution {
     public String solution(String s) {
-        // 전략 :
-        // s 에서 하나씩 잘라내서 String 배열에 담기
-        // String 배열과 길이가 같은 int 배열 선언
-        // int 배열에 String 배열값 담기
-        // 최대값과 최소값 구하기
         String answer = "";
 
-        String[] strArr = s.split(" ");
+        String[] arr = s.split(" ");
 
-        int[] intArr = new int[strArr.length];
-
-        for (int i = 0; i < strArr.length; i++) {
-            intArr[i] = Integer.parseInt(strArr[i]);
-        }
-
-        int max = intArr[0];
-
-        int min = intArr[0];
-
-        for (int i = 0; i < intArr.length; i++) {
-            if (intArr[i] > max) {
-                max = intArr[i];
-            }
-            if (intArr[i] < min) {
-                min = intArr[i];
+        for (int i = 0; i < arr.length; i++) {
+            String now = arr[i];
+            if (arr[i].length() == 0) {
+                answer += " ";
+            } else {
+                answer += now.substring(0, 1).toUpperCase();
+                answer += now.substring(1, now.length()).toLowerCase();
+                answer += " ";
             }
         }
-        answer = min + " " + max;
-
-        return answer;
+        if (s.substring(s.length() - 1, s.length()).equals(" ")) {
+            return answer;
+        }
+        return answer.substring(0, answer.length() - 1);
     }
 }
