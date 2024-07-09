c7e498c30da3c48f4b433867e76c876094338c08
kinam <maltize9292@gmail.com>
2024-07-09 09:59:43 +0900

    24.07-09 콜라츠 추측

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,10 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(10));
-        System.out.println(solution.solution(12));
-        System.out.println(solution.solution(11));
-        System.out.println(solution.solution(13));
+        System.out.println(solution.solution(6));
+        System.out.println(solution.solution(16));
+        System.out.println(solution.solution(626331));
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/12947
+// https://school.programmers.co.kr/learn/courses/30/lessons/12943
--- src/Solution.java
+++ src/Solution.java
@@ -1,25 +1,29 @@
 class Solution {
-    public boolean solution(int x) {
-        // 전략
-        // int -> String
-        // substring
-        // parseInt
-        // sum
-        boolean answer = true;
+    public int solution(int num) {
+        // 전략 :
+        // num == 1 이면 answer 리턴
+        // num % 2 == 0 이면 num/2
+        // num % 2 == 1 이면 num*3+1
+        // answer 500 넘어가면 -1 리턴
+        // 연산중에 int 범위 넘어가는 경우 나오기 때문에 (long)num 으로 형변환
+        int answer = 0;
 
-        String str = String.valueOf(x);
+        long n = (long)num;
 
-        int sum = 0;
-        for (int i = 0; i < str.length(); i++) {
-            sum += Integer.parseInt(str.substring(i, i + 1));
+        while (n != 1) {
+            if (answer <= 500) {
+                if (n % 2 == 0) {
+                    n = n / 2;
+                    answer++;
+                } else if (n % 2 == 1) {
+                    n = n * 3 + 1;
+                    answer++;
+                }
+            } else {
+                answer = -1;
+                break;
+            }
         }
-
-        if (x % sum == 0) {
-            answer = true;
-        } else {
-            answer = false;
-        }
-
         return answer;
     }
 }
