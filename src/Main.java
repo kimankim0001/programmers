62becc78cf9b9caf1d06f069d6dcc27c4159bf7d
kinam <maltize9292@gmail.com>
2024-07-10 21:12:48 +0900

    24.07-10 서울에서 김서방 찾기

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
+
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(6));
-        System.out.println(solution.solution(16));
-        System.out.println(solution.solution(626331));
+        String[] a = {"Jane", "Kim"};
+
+        System.out.println(solution.solution(a));
+
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/12943
+// https://school.programmers.co.kr/learn/courses/30/lessons/12919
--- src/Solution.java
+++ src/Solution.java
@@ -1,27 +1,13 @@
 class Solution {
-    public int solution(int num) {
+
+    public String solution(String[] seoul) {
         // 전략 :
-        // num == 1 이면 answer 리턴
-        // num % 2 == 0 이면 num/2
-        // num % 2 == 1 이면 num*3+1
-        // answer 500 넘어가면 -1 리턴
-        // 연산중에 int 범위 넘어가는 경우 나오기 때문에 (long)num 으로 형변환
-        int answer = 0;
-
-        long n = (long)num;
-
-        while (n != 1) {
-            if (answer <= 500) {
-                if (n % 2 == 0) {
-                    n = n / 2;
-                    answer++;
-                } else if (n % 2 == 1) {
-                    n = n * 3 + 1;
-                    answer++;
-                }
-            } else {
-                answer = -1;
-                break;
+        // if(i == "Kim")
+        // 인덱스 번호 반환
+        String answer = "";
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
+                answer = "김서방은 " + i + "에 있다";
             }
         }
         return answer;
