03dd67aee4a8eb99854678b1e08235829432c4ac
kinam <maltize9292@gmail.com>
2024-07-11 09:36:30 +0900

    24.07-11 나누어 떨어지는 숫자 배열

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        String[] a = {"Jane", "Kim"};
+        int[] a = {5, 9, 7, 10};
+        int[] b = {2, 36, 1, 3};
+        int[] c = {3, 2, 6};
 
-        System.out.println(solution.solution(a));
+        System.out.println(Arrays.toString(solution.solution(a, 5)));
+        System.out.println(Arrays.toString(solution.solution(b, 1)));
+        System.out.println(Arrays.toString(solution.solution(c, 10)));
+
 
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/12919
+// https://school.programmers.co.kr/learn/courses/30/lessons/12910
--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class Solution {
 
-    public String solution(String[] seoul) {
-        // 전략 :
-        // if(i == "Kim")
-        // 인덱스 번호 반환
-        String answer = "";
-        for (int i = 0; i < seoul.length; i++) {
-            if (seoul[i].equals("Kim")) {
-                answer = "김서방은 " + i + "에 있다";
+    public int[] solution(int[] arr, int divisor) {
+        // 전략
+        // ArrayList
+        // arr[] % divisor == 0 일때, add
+        // size = 0, -1 add
+        // Arrays.sort()
+
+        int[] answer = {};
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) {
+                list.add(arr[i]);
             }
         }
+
+        if (list.size() == 0) {
+            answer = new int[]{-1};
+        } else {
+            answer = new int[list.size()];
+            for (int i = 0; i < list.size(); i++) {
+                answer[i] = list.get(i);
+            }
+            Arrays.sort(answer);
+        }
+
         return answer;
     }
 }
