46996d746a7904915447a8b75b5edbd356f3c107
kinam <maltize9292@gmail.com>
2024-07-14 16:55:19 +0900

    24.07-14 없는 숫자 더하기

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,14 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("01033334444"));
-        System.out.println(solution.solution("027778888"));
+        int[] a = {1, 2, 3, 4, 6, 7, 8, 0};
+        int[] b = {5, 8, 4, 0, 6, 7, 9};
+
+        System.out.println(solution.solution(a));
+        System.out.println(solution.solution(b));
+
 
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/12948
+// https://school.programmers.co.kr/learn/courses/30/lessons/86051
--- src/Solution.java
+++ src/Solution.java
@@ -1,23 +1,13 @@
 class Solution {
-    public String solution(String phone_number) {
+
+    public int solution(int[] numbers) {
         // 전략 :
-        // 스트링 잘라서 담을 배열 선언
-        // 배열에 넣어주고
-        // 인덱스값이 뒤에서부터 4번째보다 작으면 "*" answer 에 더해주고
-        // 크거나 같으면 그냥 answer 에 하나씩 더해주기
-        String answer = "";
-        String[] num = new String[phone_number.length()];
+        // 0부터 9까지의 합 45
+        // 배열 다 더해서 빼고 리턴
+        int answer = 45;
 
-        for (int i = 0; i < phone_number.length(); i++) {
-            num[i] = String.valueOf(phone_number.charAt(i));
-        }
-
-        for (int i = 0; i < num.length; i++) {
-            if (i < num.length - 4) {
-                answer += "*";
-            } else {
-                answer += num[i];
-            }
+        for (int i = 0; i < numbers.length; i++) {
+            answer -= numbers[i];
         }
         return answer;
     }
