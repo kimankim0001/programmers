0bc1e5b25f82404590383da210f5d45014a0bf6d
kinam <maltize9292@gmail.com>
2024-07-16 18:53:16 +0900

    24.07-16 가운데 글자 가져오기

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,11 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        int[] a = {4,3,2,1};
-        int[] b = {10};
-
-        System.out.println(Arrays.toString(solution.solution(a)));
-        System.out.println(Arrays.toString(solution.solution(b)));
-
-
-
+        System.out.println(solution.solution("abcde"));
+        System.out.println(solution.solution("qwer"));
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/12935
+// https://school.programmers.co.kr/learn/courses/30/lessons/12903
--- src/Solution.java
+++ src/Solution.java
@@ -1,32 +1,17 @@
 class Solution {
-    public int[] solution(int[] arr) {
+    public String solution(String s) {
         // 전략
-        // 배열길이 0이면 -1 리턴
-        // arr 최소값 찾기
-        // 최소값 제외하고 answer 에 넣기
-        int[] answer = {};
+        // 길이가 짝수면 두글자 홀수면 한글자
+        // s.substring
+        // s.length/2-1, s.length/2+1
+        // s.length/2, s.length/2+1
+        String answer = "";
 
-        if (arr.length == 1) {
-            answer = new int[arr.length];
-            answer[0] = -1;
-            return answer;
+        if (s.length() % 2 == 0) {
+            answer = s.substring(s.length()/2-1, s.length()/2+1);
+        } else {
+            answer = s.substring(s.length()/2, s.length()/2+1);
         }
-
-        answer = new int[arr.length-1];
-
-        int min = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            min = Math.min(min, arr[i]);
-        }
-
-        int index = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != min) {
-                answer[index] = arr[i];
-                index++;
-            }
-        }
-
         return answer;
     }
 }
