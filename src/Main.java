34cce427c0f3c8102421d36675bba11eea7bd8b8
kinam <maltize9292@gmail.com>
2024-07-16 18:44:17 +0900

    24.07-15 제일 작은 수 제거하기

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        int[] a = {1, 2, 3, 4, 6, 7, 8, 0};
-        int[] b = {5, 8, 4, 0, 6, 7, 9};
+        int[] a = {4,3,2,1};
+        int[] b = {10};
 
-        System.out.println(solution.solution(a));
-        System.out.println(solution.solution(b));
+        System.out.println(Arrays.toString(solution.solution(a)));
+        System.out.println(Arrays.toString(solution.solution(b)));
+
 
 
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/86051
+// https://school.programmers.co.kr/learn/courses/30/lessons/12935
--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,32 @@
 class Solution {
+    public int[] solution(int[] arr) {
+        // 전략
+        // 배열길이 0이면 -1 리턴
+        // arr 최소값 찾기
+        // 최소값 제외하고 answer 에 넣기
+        int[] answer = {};
 
-    public int solution(int[] numbers) {
-        // 전략 :
-        // 0부터 9까지의 합 45
-        // 배열 다 더해서 빼고 리턴
-        int answer = 45;
-
-        for (int i = 0; i < numbers.length; i++) {
-            answer -= numbers[i];
+        if (arr.length == 1) {
+            answer = new int[arr.length];
+            answer[0] = -1;
+            return answer;
         }
+
+        answer = new int[arr.length-1];
+
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            min = Math.min(min, arr[i]);
+        }
+
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != min) {
+                answer[index] = arr[i];
+                index++;
+            }
+        }
+
         return answer;
     }
 }
