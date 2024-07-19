2f5b54ec3c8ae6d0cd4f469e490e8f02cb7f7598
kinam <maltize9292@gmail.com>
2024-07-19 10:37:58 +0900

    24.07-19 약수의 개수와 덧셈

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,11 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        int[] a1 = {1,2,3,4};
-        int[] a2 = {-1,0,1};
-
-        int[] b1 = {-3,-1,0,2};
-        int[] b2 = {1,0,-1};
-
-        System.out.println(solution.solution(a1, b1));
-        System.out.println(solution.solution(a2, b2));
+        System.out.println(solution.solution(13,17));
+        System.out.println(solution.solution(24,27));
 
 
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/70128
+// https://school.programmers.co.kr/learn/courses/30/lessons/77884
--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,24 @@
 class Solution {
 
-    public int solution(int[] a, int[] b) {
+    public int solution(int left, int right) {
         // 전략
-        // 내적은 두 배열의 곱을 더한것
+        // left++ <= right
+        // 약수의 개수 카운트
+        // 홀이면 -, 짝이면 +
         int answer = 0;
 
-        for (int i = 0; i < a.length; i++) {
-            answer += a[i] * b[i];
+        for (int i = left; i <= right; i++) {
+            int countNum = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    countNum++;
+                }
+            }
+            if (countNum % 2 == 0) {
+                answer += i;
+            } else {
+                answer -= i;
+            }
         }
         return answer;
     }
