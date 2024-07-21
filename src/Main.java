a6a3e843d946b13b13ce846df03e8df1ce55479b
kinam <maltize9292@gmail.com>
2024-07-21 18:16:08 +0900

    24.07-21 부족한 금액 계산하기

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,8 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("Zbcdefg"));
-
-
-
+        System.out.println(solution.solution(3,20,4));
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/12917
+// https://school.programmers.co.kr/learn/courses/30/lessons/82612
--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,25 @@
-import java.util.Arrays;
-
 class Solution {
 
-    public String solution(String s) {
+    public long solution(int price, int money, int count) {
         // 전략
-        // 배열에 넣기
-        // 정렬
-        // 문자열로 변경
-        // 내림차순으로 정렬
-        String answer = "";
-        char[] arr = s.toCharArray();
-        Arrays.sort(arr);
-        String str = new String(arr);
-        StringBuilder sb = new StringBuilder(str);
-        answer = sb.reverse().toString();
+        // 기존 이용료 price
+        // 내가 가진 돈 money
+        // 이용 횟수 count
+        // price * 1~count
+        // 부족한 금액 result
+        // 부족하지 않을 때는 0
+        long answer = 0L;
+
+        long total = 0;
+        for (int i = 1; i <= count; i++) {
+            total += price * i;
+        }
+
+        if (total > money) {
+            answer = total - money;
+        } else {
+            answer = 0;
+        }
         return answer;
     }
 }
