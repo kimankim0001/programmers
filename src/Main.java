f3230ea99607886fa9e474afcd23d852b15e3c55
kinam <maltize9292@gmail.com>
2024-07-24 16:10:08 +0900

    24.07-24 직사각형 별찍기

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,24 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        // 전략
+        // 가로길이 n, 세로길이 m, 직사각형
+        // 세로 1에서 가로 길이만큼 채우고
+        // 다음 세로칸으로 이동 가로 길이만큼 채우기를 반복
 
-        int[][] a1 = {{1, 2}, {2, 3}};
-        int[][] a2 = {{1}, {2}};
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
 
-        int[][] b1 = {{3, 4}, {5, 6}};
-        int[][] b2 = {{3}, {4}};
-
-        System.out.println(Arrays.deepToString(solution.solution(a1, b1)));
-        System.out.println(Arrays.deepToString(solution.solution(a2, b2)));
+        for(int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/12950
+// https://school.programmers.co.kr/learn/courses/30/lessons/12969
