a6b6bcfa12b4cc1eca372987f3ec8753cb8971b3
kinam <maltize9292@gmail.com>
2024-07-26 23:08:05 +0900

    24.07-26 3진법 뒤집기

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,12 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
 
-        System.out.println(Arrays.toString(solution.solution(3,12)));
-        System.out.println(Arrays.toString(solution.solution(2,5)));
-
-
-
+        System.out.println(solution.solution(45));
+        System.out.println(solution.solution(125));
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/12940
+// https://school.programmers.co.kr/learn/courses/30/lessons/68935
--- src/Solution.java
+++ src/Solution.java
@@ -1,35 +1,17 @@
 class Solution {
 
-    public int[] solution(int n, int m) {
+    public int solution(int n) {
         // 전략
-        // 큰수에서 작은수를 나눈 나머지 -> 작은수를 나머지로 나눈 나머지가 0이 될때까지 반복 -> 나머지가 0이 되면 작은수가 최대공약수
-        // 두 수의 곱 / 최대공약수 -> 최소공배수
-        // answer[0] 최대공약수
-        // answer[1] 최소공배수
-        int[] answer = new int[2];
+        // n%3 을 String 타입의 변수에 반복해서 할당
+        // N진법 -> 10진법 으로 바꾸는 Integer.parseInt(i, N); 활용
+        int answer = 0;
 
-        // 초기값 간직하기 위해 a 와 b 에 할당
-        int a = n;
-        int b = m;
-
-        if (n < m) {
-            int temp = n;
-            n = m;
-            m = temp;
+        String str = "";
+        while (n != 0) {
+            str += n % 3;
+            n /= 3;
         }
 
-        int r = 0;
-
-        while (m > 0) {
-            r = n % m;
-            n = m;
-            m = r;
-        }
-
-        answer[0] = n;
-
-        answer[1] = a * b / n;
-
-        return answer;
+        return answer = Integer.parseInt(str, 3);
     }
 }
