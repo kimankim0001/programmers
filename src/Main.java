db241771e93474795035e3b22f9b07dc69640e4b
kinam <maltize9292@gmail.com>
2024-07-29 17:57:58 +0900

    24.07-29 크기가 작은 부분문자열

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,10 @@ public class Main {
 
         Solution solution = new Solution();
 
-        int[] a = {-2, 3, 0, 2, -5};
-        int[] b = {-3, -2, -1, 0, 1, 2, 3};
-        int[] c = {-1, 1, -1, 1};
-
-        System.out.println(solution.solution(a));
-        System.out.println(solution.solution(b));
-        System.out.println(solution.solution(c));
+        System.out.println(solution.solution("3141592", "271"));
+        System.out.println(solution.solution("500220839878", "7"));
+        System.out.println(solution.solution("10203", "15"));
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/131705
+// https://school.programmers.co.kr/learn/courses/30/lessons/147355
--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,26 @@
 class Solution {
 
-    public int solution(int[] number) {
+    public int solution(String t, String p) {
         // 전략
-        // 세명의 번호의 합이 0 일때 삼총사 ++
-        // 3중 for 문
+        // p의 길이만큼 t에서 잘랐을때
+        // t의 잘린값이 p의 값보다 작거나 같으면 answer++
+        // String 타입이므로 비교를 하기 위해선 Int 타입으로 형변환
+        // t.length()-p.length()+1
+        // p의 길이만큼 t에서 substring 하고 비교해보기
+        // Int 타입으로 하니까 런타임 에러, Long 타입으로 형변환
+        // 1 ≤ p의 길이 ≤ 18
+
         int answer = 0;
 
-        for (int i = 0; i < number.length; i++) {
-            for (int j = i + 1; j < number.length; j++) {
-                for (int k = j + 1; k < number.length; k++) {
-                    if (number[i] + number[j] + number[k] == 0) {
-                        answer++;
-                    }
-                }
+        long pp = Long.parseLong(p);
+
+        for (int i = 0; i < t.length() - p.length() + 1; i++) {
+            long tt = Long.parseLong(t.substring(i, i + p.length()));
+            if (tt <= pp) {
+                answer++;
             }
         }
+
         return answer;
     }
 }
