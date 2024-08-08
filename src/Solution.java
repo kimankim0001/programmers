7e0f93c3b79194d35311756abd8810f43f707863
kinam <maltize9292@gmail.com>
2024-08-08 19:50:33 +0900

    24.08-08 명예의 전당

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 class Solution {
 
-    public int solution(int a, int b, int n) {
+    public int[] solution(int k, int[] score) {
         // 전략
-        // a 개를 주면 b 개를 돌려주는데에 n 개를 가져갔을때의 리턴
-        // answer += n / a * b 상빈이가 받는 새콜라
-        // n = (n / a * b) + (n % a) 상빈이가 가지고 있는 콜라
-        // n >= a 콜라를 돌려 받을 수 있는 최소 조건
+        // 랭킹을 k 만큼 담고 최소값을 리턴하다가
+        // 담은 랭킹의 수가 k 보다 커지면 최소값을 없애고 다시 최소값을 리턴
+        int[] answer = new int[score.length];
 
-        int answer = 0;
+        List<Integer> rank = new ArrayList<>();
 
-        while (n >= a) {
-            answer += n / a * b;
-            n = (n / a * b) + (n % a);
+        for (int i = 0; i < score.length; i++) {
+            rank.add(score[i]);
+            if (rank.size() > k) {
+                rank.remove(Collections.min(rank));
+            }
+            answer[i] = Collections.min(rank);
         }
+
         return answer;
     }
 }
