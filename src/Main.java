94dc26051e090dad31b40edca325e1583e6dff15
kinam <maltize9292@gmail.com>
2024-08-13 09:18:18 +0900

    24.08-13 소수 만들기

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,12 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
 
-        int[] a = {1, 2, 3, 4, 5};
-        int[] b = {1, 3, 2, 4, 2};
-
-        System.out.println(Arrays.toString(solution.solution(a)));
-        System.out.println(Arrays.toString(solution.solution(b)));
-
+        System.out.println(solution.solution(new int[]{1, 2, 3, 4}));
+        System.out.println(solution.solution(new int[]{1, 2, 7, 6, 4}));
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/42840
+// https://school.programmers.co.kr/learn/courses/30/lessons/12977
