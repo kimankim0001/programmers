612ae0195ee1c0ab763fcb368dbd8e0298ea04bf
kinam <maltize9292@gmail.com>
2024-08-14 21:17:34 +0900

    24.08-14

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ public class Main {
 
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{1, 2, 3, 4}));
-        System.out.println(solution.solution(new int[]{1, 2, 7, 6, 4}));
+        System.out.println(solution.solution(8,4,new int[]{2,3,6}));
+        System.out.println(solution.solution(5,4,new int[]{1,3}));
+        System.out.println(solution.solution(4,1,new int[]{1,2,3,4}));
+
+
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/12977
+// https://school.programmers.co.kr/learn/courses/30/lessons/161989
--- src/Solution.java
+++ src/Solution.java
@@ -1,34 +1,18 @@
 class Solution {
 
-    public int solution(int[] nums) {
-        //전략
-        // 3개의 수 더해서 소수가 되는 경우의 수
-        // 소수 검증 매서드 사용
-
-        int answer = 0;
-
-        int sum = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    sum = nums[i] + nums[j] + nums[k];
-                    if (isSosu(sum)) {
-                        answer++;
-                    }
-                }
+    public int solution(int n, int m, int[] section) {
+        // 전략
+        // 페인트가 칠해진 길이 n
+        // 페인트를 칠하는 롤러의 길이 m
+        // 페인트를 칠해야 하는 1미터 단위 구역 section
+        int answer = 1;
+        int index = section[0];
+        for (int i = 1; i < section.length; i++) {
+            if (index + m - 1 < section[i]) {
+                answer++;
+                index = section[i];
             }
         }
         return answer;
-    }
-
-    // 소수 검증 매서드
-    private boolean isSosu(int n) {
-        for (int i = 2; i < n; i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
     }
 }
