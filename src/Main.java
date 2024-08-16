42d30f13485693bd0d894a92a1191f2be487f2e8
kinam <maltize9292@gmail.com>
2024-08-16 09:08:52 +0900

    24.08-16 로또의 최고 순위와 최저 순위

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(5,3,2));
-        System.out.println(solution.solution(10,3,2));
+        int[] l1 = {44, 1, 0, 0, 31, 25};
+        int[] l2 = {0, 0, 0, 0, 0, 0};
+        int[] l3 = {45, 4, 35, 20, 3, 9};
+
+        int[] w1 = {31, 10, 45, 1, 6, 19};
+        int[] w2 = {38, 19, 20, 40, 15, 25};
+        int[] w3 = {20, 9, 3, 45, 4, 35};
+
+        System.out.println(Arrays.toString(solution.solution(l1, w1)));
+        System.out.println(Arrays.toString(solution.solution(l2, w2)));
+        System.out.println(Arrays.toString(solution.solution(l3, w3)));
 
 
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/136798
+// https://school.programmers.co.kr/learn/courses/30/lessons/77484
--- src/Solution.java
+++ src/Solution.java
@@ -1,29 +1,34 @@
 class Solution {
 
-    public int solution(int number, int limit, int power) {
+    public int[] solution(int[] lottos, int[] win_nums) {
         // 전략
-        // 기사 1~number 기사 번호
-        // 기사 번호 약수 개수 = 기사 공격력
-        // 협약 공격력 limit 넘을경우 협약기관에서 정한 공격력 power
-        // 기사들의 총 공격력 answer
-
-        int answer = 0;
-
-        for (int i = 1; i <= number; i++) {
-            int cnt = 0;
-            for (int j = 1; j * j <= i; j++) {
-                if (j * j == i) {
-                    cnt++;
-                } else if (i % j == 0) {
-                    cnt += 2;
+        // answer 최고 1위 최저 7위 선언
+        // 민우 번호에 0 이 있으면 그만큼 최고 순위에 마이너스 적용
+        // 이중 for 문으로 당첨번호와 민우 번호 비교
+        // 번호가 같으면 최저에 플러스
+        // 번호가 다르면 최고에 마니어스
+        // 최고 최저 7위이면 6위로 적용
+        int[] answer = {1, 7};
+        for (int i = 0; i < 6; i++) {
+            if (lottos[i] == 0) {
+                answer[0]--;
+            }
+        }
+        for (int i = 0; i < 6; i++) {
+            answer[0]++;
+            for (int j = 0; j < 6; j++) {
+                if (win_nums[i] == lottos[j]) {
+                    answer[1]--;
+                    answer[0]--;
                 }
             }
-            if (cnt > limit) {
-                cnt = power;
-            }
-            answer += cnt;
         }
-
+        if (answer[0] == 7) {
+            answer[0] = 6;
+        }
+        if (answer[1] == 7) {
+            answer[1] = 6;
+        }
         return answer;
     }
 }
