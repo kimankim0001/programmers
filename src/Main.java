8e7c2fbff656f6216ffdbb5ca60be0d56db74bbb
kinam <maltize9292@gmail.com>
2024-08-17 12:44:34 +0900

    24.08-17 옹알이2

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,17 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
 
-        int[] l1 = {44, 1, 0, 0, 31, 25};
-        int[] l2 = {0, 0, 0, 0, 0, 0};
-        int[] l3 = {45, 4, 35, 20, 3, 9};
+        String[] a = {"aya", "yee", "u", "maa"};
+        String[] b = {"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"};
 
-        int[] w1 = {31, 10, 45, 1, 6, 19};
-        int[] w2 = {38, 19, 20, 40, 15, 25};
-        int[] w3 = {20, 9, 3, 45, 4, 35};
-
-        System.out.println(Arrays.toString(solution.solution(l1, w1)));
-        System.out.println(Arrays.toString(solution.solution(l2, w2)));
-        System.out.println(Arrays.toString(solution.solution(l3, w3)));
+        System.out.println(solution.solution(a));
+        System.out.println(solution.solution(b));
 
 
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/77484
+// https://school.programmers.co.kr/learn/courses/30/lessons/133499
--- src/Solution.java
+++ src/Solution.java
@@ -1,33 +1,24 @@
 class Solution {
 
-    public int[] solution(int[] lottos, int[] win_nums) {
-        // 전략
-        // answer 최고 1위 최저 7위 선언
-        // 민우 번호에 0 이 있으면 그만큼 최고 순위에 마이너스 적용
-        // 이중 for 문으로 당첨번호와 민우 번호 비교
-        // 번호가 같으면 최저에 플러스
-        // 번호가 다르면 최고에 마이너스
-        // 최고 최저 7위이면 6위로 적용
-        int[] answer = {1, 7};
-        for (int i = 0; i < 6; i++) {
-            if (lottos[i] == 0) {
-                answer[0]--;
+    public int solution(String[] babbling) {
+        // 전략 :
+        // 반복되는 발음 "ayaaya", "yeye", "woowoo", "mama" 가 있으면 "X" 로 만들기
+        // babbling 문자열에 "aya", "ye", "woo", "ma" 가 있으면 공백으로 만들기
+        // 공백을 지웠을때 해당 문자열의 길이가 0이면 answer++;
+
+        int answer = 0;
+
+        String[] str = {"aya", "ye", "woo", "ma"};
+
+        String[] repeatStr = {"ayaaya", "yeye", "woowoo", "mama"};
+
+        for (int i = 0; i < babbling.length; i++) {
+            for (int j = 0; j < str.length; j++) {
+                babbling[i] = babbling[i].replaceAll(repeatStr[j], "X").replaceAll(str[j], " ");
             }
-        }
-        for (int i = 0; i < 6; i++) {
-            answer[0]++;
-            for (int j = 0; j < 6; j++) {
-                if (win_nums[i] == lottos[j]) {
-                    answer[1]--;
-                    answer[0]--;
-                }
+            if (babbling[i].trim().length() == 0) {
+                answer++;
             }
-        }
-        if (answer[0] == 7) {
-            answer[0] = 6;
-        }
-        if (answer[1] == 7) {
-            answer[1] = 6;
         }
         return answer;
     }
