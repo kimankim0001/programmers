b4d17eeae697a00f25992d609b6c472576a64f54
kinam <maltize9292@gmail.com>
2024-08-21 09:14:01 +0900

    24.08-21 자연수 뒤집어 배열로 만들기

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,9 @@ public class Main {
 
         Solution solution = new Solution();
 
-        System.out.println(Arrays.toString(solution.solution(2, 5)));
-        System.out.println(Arrays.toString(solution.solution(4, 3)));
-        System.out.println(Arrays.toString(solution.solution(-4, 2)));
+        System.out.println(Arrays.toString(solution.solution(12345)));
 
     }
 }
 // 문제 링크
-// https://school.programmers.co.kr/learn/courses/30/lessons/12954
+// https://school.programmers.co.kr/learn/courses/30/lessons/12932
--- src/Solution.java
+++ src/Solution.java
@@ -1,11 +1,13 @@
 class Solution {
 
-    public long[] solution(int x, int n) {
-        long[] answer = new long[n];
-        long a = 0;
-        for (int i = 0; i < n; i++) {
-            answer[i] = a + x;
-            a = a + x;
+    public int[] solution(long n) {
+        // String 타입으로 변환
+        // 배열의 길이 선언
+        // 끝부터 순회하며 파싱
+        String str = String.valueOf(n);
+        int[] answer = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            answer[i] = Integer.parseInt(str.substring(str.length() - 1 - i, str.length() - i));
         }
         return answer;
     }
