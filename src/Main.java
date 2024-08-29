7905e2dedc83ec75bd7ff58e90249d62a0860fa0
kinam <maltize9292@gmail.com>
2024-08-29 17:20:10 +0900

    24-08-29 n의 배수 고르기

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,13 @@ public class Main {
 
         Solution solution = new Solution();
 
-        int[] a1 = {2, 1, 6};
-        int[] a2 = {5, 2, 1, 7, 5};
+        int[] a1 = {4, 5, 6, 7, 8, 9, 10, 11, 12};
+        int[] a2 = {1, 9, 3, 10, 13, 5};
+        int[] a3 = {2, 100, 120, 600, 12, 12};
 
-        System.out.println(Arrays.toString(solution.solution(a1)));
-        System.out.println(Arrays.toString(solution.solution(a2)));
+        System.out.println(Arrays.toString(solution.solution(3, a1)));
+        System.out.println(Arrays.toString(solution.solution(5, a2)));
+        System.out.println(Arrays.toString(solution.solution(12, a3)));
 
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,21 @@
 class Solution {
 
-    public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length + 1];
-        for (int i = 0; i < num_list.length; i++) {
-            answer[i] = num_list[i];
+    public int[] solution(int n, int[] numlist) {
+        int count = 0;
+
+        for (int i = 0; i < numlist.length; i++) {
+            if (numlist[i] % n == 0) {
+                count++;
+            }
         }
-        if (num_list[num_list.length - 1] > num_list[num_list.length - 2]) {
-            answer[num_list.length] = num_list[num_list.length - 1] - num_list[num_list.length - 2];
-        } else {
-            answer[num_list.length] = num_list[num_list.length - 1] * 2;
+        int[] answer = new int[count];
+
+        int index = 0;
+        for (int i = 0; i < numlist.length; i++) {
+            if (numlist[i] % n == 0) {
+                answer[index] = numlist[i];
+                index++;
+            }
         }
         return answer;
     }
