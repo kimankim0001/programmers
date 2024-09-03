b5f4abd06d35b245809aa990607d2c6a56c3ebbf
kinam <maltize9292@gmail.com>
2024-09-03 16:36:47 +0900

    24-09-03 배열 만들기 6

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Solution s = new Solution();
 
-        int[] a1 = {1, 2, 3, 4};
-        int[] a2 = {2, 4, 8};
+        int[] a1 = {0, 1, 1, 1, 0};
+        int[] a2 = {0, 1, 0, 1, 0};
+        int[] a3 = {0, 1, 1, 0};
 
-        System.out.println(s.solution(a1));
-        System.out.println(s.solution(a2));
+        System.out.println(Arrays.toString(s.solution(a1)));
+        System.out.println(Arrays.toString(s.solution(a2)));
+        System.out.println(Arrays.toString(s.solution(a3)));
 
     }
 }
