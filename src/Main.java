3e36030822eface8943974093c61501b1aa05d9e
kinam <maltize9292@gmail.com>
2024-09-04 14:06:51 +0900

    24-09-04 중복된 숫자 개수

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,13 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
 
         Solution s = new Solution();
 
-        int[] a1 = {0, 1, 1, 1, 0};
-        int[] a2 = {0, 1, 0, 1, 0};
-        int[] a3 = {0, 1, 1, 0};
+        int[] a1 = {1, 1, 2, 3, 4, 5};
+        int[] a2 = {0, 2, 3, 4};
 
-        System.out.println(Arrays.toString(s.solution(a1)));
-        System.out.println(Arrays.toString(s.solution(a2)));
-        System.out.println(Arrays.toString(s.solution(a3)));
-
+        System.out.println(s.solution(a1, 1));
+        System.out.println(s.solution(a2, 1));
     }
 }
