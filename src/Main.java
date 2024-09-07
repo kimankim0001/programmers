afe8e596858f3641fa038abf783b9d2ed508873c
kinam <maltize9292@gmail.com>
2024-09-07 18:38:28 +0900

    24-09-07 완주하지 못한 선수

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Solution s = new Solution();
+        Solution solution = new Solution();
 
-        System.out.println(s.solution("10", "11"));
-        System.out.println(s.solution("1001", "1111"));
+        String[] a1 = {"leo", "kiki", "eden"};
+        String[] a2 = {"marina", "josipa", "nikola", "vinko", "filipa"};
+        String[] a3 = {"mislav", "stanko", "mislav", "ana"};
+
+        String[] b1 = {"eden", "kiki"};
+        String[] b2 = {"josipa", "filipa", "marina", "nikola"};
+        String[] b3 = {"stanko", "ana", "mislav"};
+
+        System.out.println(solution.solution(a1, b1));
+        System.out.println(solution.solution(a2, b2));
+        System.out.println(solution.solution(a3, b3));
+
     }
 }
