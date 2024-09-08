73a0542d9add6e696ac310bc9cbd042ce7450057
kinam <maltize9292@gmail.com>
2024-09-08 12:10:52 +0900

    24-09-08 그림 확대

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
 
-        String[] a1 = {"leo", "kiki", "eden"};
-        String[] a2 = {"marina", "josipa", "nikola", "vinko", "filipa"};
-        String[] a3 = {"mislav", "stanko", "mislav", "ana"};
+        String[] a1 = {".xx...xx.", "x..x.x..x", "x...x...x", ".x.....x.", "..x...x..", "...x.x...", "....x...."};
+        String[] a2 = {"x.x", ".x.", "x.x"};
 
-        String[] b1 = {"eden", "kiki"};
-        String[] b2 = {"josipa", "filipa", "marina", "nikola"};
-        String[] b3 = {"stanko", "ana", "mislav"};
-
-        System.out.println(solution.solution(a1, b1));
-        System.out.println(solution.solution(a2, b2));
-        System.out.println(solution.solution(a3, b3));
-
+        System.out.println(Arrays.toString(solution.solution(a1, 2)));
+        System.out.println(Arrays.toString(solution.solution(a2, 3)));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,31 +1,19 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
 
-    public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        // 같은 이름의 완주자가 있을 수 있기 때문에 map 사용
-        Map<String, Integer> map = new HashMap<>();
-
-        for (String s : participant) {
-            // key가 없는 경우 1, 있을 경우 +1
-            map.put(s, map.getOrDefault(s, 0) + 1);
-        }
-
-        for (String s : completion) {
-            // 완주한 사람의 1명씩 제거
-            map.put(s, map.get(s) - 1);
-        }
-
-        for (String s : map.keySet()) {
-            // 완주하지 못한 사람
-            if (map.get(s) > 0) {
-                answer = s;
-                break;
+    public String[] solution(String[] picture, int k) {
+        String[] answer = new String[picture.length * k];
+        int index = 0;
+        for (int i = 0; i < picture.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < picture[i].length(); j++) {
+                for (int n = 0; n < k; n++) {
+                    sb.append(picture[i].charAt(j));
+                }
+            }
+            for (int j = 0; j < k; j++) {
+                answer[index++] = sb.toString();
             }
         }
-
         return answer;
     }
 }
