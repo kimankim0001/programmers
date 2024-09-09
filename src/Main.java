95d2ccc0958c6f57b4d9535b3f2d974d600d8355
kinam <maltize9292@gmail.com>
2024-09-09 14:29:04 +0900

    카운트 업

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,6 @@ public class Main {
 
         Solution solution = new Solution();
 
-        String[] a1 = {".xx...xx.", "x..x.x..x", "x...x...x", ".x.....x.", "..x...x..", "...x.x...", "....x...."};
-        String[] a2 = {"x.x", ".x.", "x.x"};
-
-        System.out.println(Arrays.toString(solution.solution(a1, 2)));
-        System.out.println(Arrays.toString(solution.solution(a2, 3)));
+        System.out.println(Arrays.toString(solution.solution(3, 10)));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,9 @@
 class Solution {
 
-    public String[] solution(String[] picture, int k) {
-        String[] answer = new String[picture.length * k];
-        int index = 0;
-        for (int i = 0; i < picture.length; i++) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < picture[i].length(); j++) {
-                for (int n = 0; n < k; n++) {
-                    sb.append(picture[i].charAt(j));
-                }
-            }
-            for (int j = 0; j < k; j++) {
-                answer[index++] = sb.toString();
-            }
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[end_num - start_num + 1];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = start_num + i;
         }
         return answer;
     }
