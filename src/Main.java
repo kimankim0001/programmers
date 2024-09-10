dbe8a7c22acfac9ce346b03b257c40954a69aff4
kinam <maltize9292@gmail.com>
2024-09-10 11:35:42 +0900

    24-09-10 배열의 유사도

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
 
-        System.out.println(Arrays.toString(solution.solution(3, 10)));
+        String[] a1 = {"a", "b", "c"};
+        String[] a2 = {"n", "omg"};
+
+        String[] b1 = {"com", "b", "d", "p", "c"};
+        String[] b2 = {"m", "dot"};
+
+        System.out.println(solution.solution(a1,b1));
+        System.out.println(solution.solution(a2,b2));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,9 +1,13 @@
 class Solution {
 
-    public int[] solution(int start_num, int end_num) {
-        int[] answer = new int[end_num - start_num + 1];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = start_num + i;
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        for (int i = 0; i < s1.length; i++) {
+            for (int j = 0; j < s2.length; j++) {
+                if (s1[i].equals(s2[j])) {
+                    answer++;
+                }
+            }
         }
         return answer;
     }
