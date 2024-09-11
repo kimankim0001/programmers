22bbb8a60415bd22cc6420f4c57cb918b48a4fa1
kinam <maltize9292@gmail.com>
2024-09-11 15:06:06 +0900

    24-09-11 정수를 나선형으로 배치하기

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
 
-        String[] a1 = {"a", "b", "c"};
-        String[] a2 = {"n", "omg"};
-
-        String[] b1 = {"com", "b", "d", "p", "c"};
-        String[] b2 = {"m", "dot"};
-
-        System.out.println(solution.solution(a1,b1));
-        System.out.println(solution.solution(a2,b2));
+        System.out.println(Arrays.deepToString(solution.solution(4)));
+        System.out.println(Arrays.deepToString(solution.solution(5)));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,46 @@
 class Solution {
 
-    public int solution(String[] s1, String[] s2) {
-        int answer = 0;
-        for (int i = 0; i < s1.length; i++) {
-            for (int j = 0; j < s2.length; j++) {
-                if (s1[i].equals(s2[j])) {
-                    answer++;
+    public int[][] solution(int n) {
+        int[][] answer = new int[n][n];
+        int value = 1;
+        int row = 0;
+        int col = 0;
+        int direction = 0;
+
+        while (value <= n * n) {
+            answer[row][col] = value++;
+
+            if (direction == 0) {
+                if (col == n - 1 || answer[row][col + 1] != 0) {
+                    direction = 1;
+                    row++;
+                } else {
+                    col++;
+                }
+            } else if (direction == 1) {
+                if (row == n - 1 || answer[row + 1][col] != 0) {
+                    direction = 2;
+                    col--;
+                } else {
+                    row++;
+                }
+            } else if (direction == 2) {
+                if (col == 0 || answer[row][col - 1] != 0) {
+                    direction = 3;
+                    row--;
+                } else {
+                    col--;
+                }
+            } else if (direction == 3) {
+                if (row == 0 || answer[row - 1][col] != 0) {
+                    direction = 0;
+                    col++;
+                } else {
+                    row--;
                 }
             }
         }
+
         return answer;
     }
 }
