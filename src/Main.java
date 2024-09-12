2263ebb86077757defd71a3d13f94c0f04ecf566
kinam <maltize9292@gmail.com>
2024-09-12 14:05:25 +0900

    24-09-12 가위 바위 보

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
 
-        System.out.println(Arrays.deepToString(solution.solution(4)));
-        System.out.println(Arrays.deepToString(solution.solution(5)));
+        System.out.println(solution.solution("2"));
+        System.out.println(solution.solution("205"));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,46 +1,18 @@
 class Solution {
 
-    public int[][] solution(int n) {
-        int[][] answer = new int[n][n];
-        int value = 1;
-        int row = 0;
-        int col = 0;
-        int direction = 0;
+    public String solution(String rsp) {
+        String answer = "";
+        String[] arr = rsp.split("");
 
-        while (value <= n * n) {
-            answer[row][col] = value++;
-
-            if (direction == 0) {
-                if (col == n - 1 || answer[row][col + 1] != 0) {
-                    direction = 1;
-                    row++;
-                } else {
-                    col++;
-                }
-            } else if (direction == 1) {
-                if (row == n - 1 || answer[row + 1][col] != 0) {
-                    direction = 2;
-                    col--;
-                } else {
-                    row++;
-                }
-            } else if (direction == 2) {
-                if (col == 0 || answer[row][col - 1] != 0) {
-                    direction = 3;
-                    row--;
-                } else {
-                    col--;
-                }
-            } else if (direction == 3) {
-                if (row == 0 || answer[row - 1][col] != 0) {
-                    direction = 0;
-                    col++;
-                } else {
-                    row--;
-                }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("2")) {
+                answer += "0";
+            } else if (arr[i].equals("0")) {
+                answer += "5";
+            } else {
+                answer += "2";
             }
         }
-
         return answer;
     }
 }
