eea5b5cdec4117404a00a5c7a66013a5ec8ba90c
kinam <maltize9292@gmail.com>
2024-09-15 10:22:34 +0900

    24-09-15 배열 회전시키기

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
 
-        int[][] a1 = {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}};
-        int[] a2 = {1, 5, 3, 5, 1, 2, 1, 4};
+        int[] a1 = {1, 2, 3};
+        int[] a2 = {4, 455, 6, 4, -1, 45, 6};
 
-        System.out.println(solution.solution(a1, a2));
+        System.out.println(Arrays.toString(solution.solution(a1,"right")));
+        System.out.println(Arrays.toString(solution.solution(a2,"left")));
 
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,32 +1,19 @@
-import java.util.Stack;
-
 class Solution {
 
-    public int solution(int[][] board, int[] moves) {
-        int answer = 0;
-
-        Stack<Integer> stack = new Stack<>();
-
-        for (int move : moves) {
-            int col = move - 1;
-
-            for (int i = 0; i < board.length; i++) {
-                if (board[i][col] == 0) {
-                    continue;
-                }
-
-                if (!stack.isEmpty() && stack.peek() == board[i][col]) {
-                    stack.pop();
-                    board[i][col] = 0;
-                    answer += 2;
-                    break;
-                } else {
-                    stack.push(board[i][col]);
-                    board[i][col] = 0;
-                    break;
-                }
+    public int[] solution(int[] numbers, String direction) {
+        int[] answer = new int[numbers.length];
+        if (direction.equals("right")) {
+            answer[0] = numbers[numbers.length - 1];
+            for (int i = 0; i < numbers.length - 1; i++) {
+                answer[i + 1] = numbers[i];
+            }
+        } else if (direction.equals("left")) {
+            answer[numbers.length - 1] = numbers[0];
+            for (int i = 0; i < numbers.length - 1; i++) {
+                answer[i] = numbers[i + 1];
             }
         }
+
         return answer;
     }
 }
