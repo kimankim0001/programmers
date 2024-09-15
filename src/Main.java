3cb7f4bb7d9c1efaf0840890e651b7d20e5217f1
kinam <maltize9292@gmail.com>
2024-09-16 08:23:08 +0900

    24-09-16 특별한 이차원 배열 1

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,8 @@ public class Main {
 
         Solution solution = new Solution();
 
-        int[] a1 = {1, 2, 3};
-        int[] a2 = {4, 455, 6, 4, -1, 45, 6};
-
-        System.out.println(Arrays.toString(solution.solution(a1,"right")));
-        System.out.println(Arrays.toString(solution.solution(a2,"left")));
-
+        System.out.println(Arrays.deepToString(solution.solution(3)));
+        System.out.println(Arrays.deepToString(solution.solution(6)));
+        System.out.println(Arrays.deepToString(solution.solution(1)));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,16 @@
 class Solution {
 
-    public int[] solution(int[] numbers, String direction) {
-        int[] answer = new int[numbers.length];
-        if (direction.equals("right")) {
-            answer[0] = numbers[numbers.length - 1];
-            for (int i = 0; i < numbers.length - 1; i++) {
-                answer[i + 1] = numbers[i];
-            }
-        } else if (direction.equals("left")) {
-            answer[numbers.length - 1] = numbers[0];
-            for (int i = 0; i < numbers.length - 1; i++) {
-                answer[i] = numbers[i + 1];
+    public int[][] solution(int n) {
+        int[][] answer = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    answer[i][j] = 1;
+                } else {
+                    answer[i][j] = 0;
+                }
             }
         }
-
         return answer;
     }
 }
