fe305080de386eecaf7d409e0e7bf935191cb42b
kinam <maltize9292@gmail.com>
2024-09-19 18:13:26 +0900

    24-09-19 신규 아이디 추천

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(10));
-        System.out.println(solution.solution(5));
+        System.out.println(solution.solution("...!@BaT#*..y.abcdefghijklm"));
+        System.out.println(solution.solution("z-+.^."));
+        System.out.println(solution.solution("=.="));
+        System.out.println(solution.solution("123_.def"));
+        System.out.println(solution.solution("abcdefghijklmn.p"));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,21 +1,37 @@
 class Solution {
 
-    public int solution(int n) {
-        int answer = 1;
-        for (int i = 3; i <= n; i++) {
-            if (isPrime(i)) {
-                answer += 1;
-            }
-        }
-        return answer;
-    }
+    public String solution(String new_id) {
+        String answer = new_id.toLowerCase();
+        answer = answer.replaceAll("[^a-z0-9-_.]", "");
 
-    private boolean isPrime(int num) {
-        for (int i = 2; i <= (int) Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                return false;
+        while (answer.contains("..")) {
+            answer = answer.replace("..", ".");
+        }
+
+        if (answer.charAt(0) == '.') {
+            answer = answer.substring(1);
+        }
+        if (answer.length() > 0 && answer.charAt(answer.length() - 1) == '.') {
+            answer = answer.substring(0, answer.length() - 1);
+        }
+
+        if (answer.length() == 0) {
+            answer = "a";
+        }
+
+        if (answer.length() > 15) {
+            answer = answer.substring(0, 15);
+            if (answer.charAt(answer.length() - 1) == '.') {
+                answer = answer.substring(0, answer.length() - 1);
             }
         }
-        return true;
+
+        if (answer.length() == 1) {
+            answer += String.valueOf(answer.charAt(0)) + String.valueOf(answer.charAt(0));
+        } else if (answer.length() == 2) {
+            answer += String.valueOf(answer.charAt(1));
+        }
+
+        return answer;
     }
 }
