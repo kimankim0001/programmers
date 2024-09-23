f68c3d0b7fe00bbb10301fce660b25fa6f512c80
kinam <maltize9292@gmail.com>
2024-09-23 14:46:57 +0900

    24-09-23 n의 배수

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,7 @@ public class Main {
 
         Solution solution = new Solution();
 
-        int[] a = {1, 3, 2, 5, 4};
-        int[] b = {2, 2, 3, 3};
-
-        System.out.println(solution.solution(a, 9));
-        System.out.println(solution.solution(b, 10));
+        System.out.println(solution.solution(98, 2));
+        System.out.println(solution.solution(34, 3));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,11 @@
-import java.util.Arrays;
-
 class Solution {
 
-    public int solution(int[] d, int budget) {
+    public int solution(int num, int n) {
         int answer = 0;
-        int hap = 0;
-
-        Arrays.sort(d);
-        for (int i : d) {
-            hap += i;
-            if (hap > budget) {
-                continue;
-            }
-            answer++;
+        if (num % n == 0) {
+            answer = 1;
+        } else {
+            answer = 0;
         }
         return answer;
     }
