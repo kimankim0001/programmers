90fa444d617f57aa9a98abd671dcd781d9846089
kinam <maltize9292@gmail.com>
2024-09-29 17:28:17 +0900

    24-09-29 n보다 커질 때까지 더하기

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new String[] { "a", "b", "c" }));
+        int[] a1 = {34, 5, 71, 29, 100, 34};
+        int[] a2 = {58, 44, 27, 10, 100};
+
+        System.out.println(solution.solution(a1, 123));
+        System.out.println(solution.solution(a2, 139));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,9 +1,13 @@
 class Solution {
 
-    public String solution(String[] arr) {
-        String answer = "";
-        for (int i = 0; i < arr.length; i++) {
-            answer += arr[i];
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (answer > n) {
+                break;
+            } else {
+                answer += numbers[i];
+            }
         }
         return answer;
     }
