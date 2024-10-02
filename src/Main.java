0fd211dc0451fe02503b3ec122d5956b198cb48a
kinam <maltize9292@gmail.com>
2024-10-02 13:16:03 +0900

    24-10-02 추억 점수

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,20 @@ public class Main {
 
         Solution solution = new Solution();
 
-        String[] a1 = {"AAA", "BBB", "CCC", "DDD"};
-        String[] a2 = {"aBc", "AbC"};
+        String[] n1 = {"may", "kein", "kain", "radi"};
+        String[] n2 = {"kali", "mari", "don"};
+        String[] n3 = {"may", "kein", "kain", "radi"};
 
-        System.out.println(Arrays.toString(solution.solution(a1)));
-        System.out.println(Arrays.toString(solution.solution(a2)));
+        int[] y1 = {5, 10, 1, 3};
+        int[] y2 = {11, 1, 55};
+        int[] y3 = {5, 10, 1, 3};
+
+        String[][] p1 = {{"may", "kein", "kain", "radi"}, {"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}};
+        String[][] p2 = {{"kali", "mari", "don"}, {"pony", "tom", "teddy"}, {"con", "mona", "don"}};
+        String[][] p3 = {{"may"}, {"kein", "deny", "may"}, {"kon", "coni"}};
+
+        System.out.println(Arrays.toString(solution.solution(n1, y1, p1)));
+        System.out.println(Arrays.toString(solution.solution(n2, y2, p2)));
+        System.out.println(Arrays.toString(solution.solution(n3, y3, p3)));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,14 @@
 class Solution {
 
-    public String[] solution(String[] strArr) {
-        String[] answer = new String[strArr.length];
-
-        for (int i = 0; i < strArr.length; i++) {
-            if (i % 2 == 0) {
-                answer[i] = strArr[i].toLowerCase();
-            } else {
-                answer[i] = strArr[i].toUpperCase();
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < photo[i].length; j++) {
+                for (int k = 0; k < name.length; k++) {
+                    if (photo[i][j].equals(name[k])) {
+                        answer[i] += yearning[k];
+                    }
+                }
             }
         }
         return answer;
