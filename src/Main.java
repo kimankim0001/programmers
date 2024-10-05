1aa8427af3eb27fc5e6b4a84a42ca84c1a547948
kinam <maltize9292@gmail.com>
2024-10-05 12:11:05 +0900

    24-10-05 키패드 누르기

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,12 @@ public class Main {
 
         Solution solution = new Solution();
 
-        int[] a1 = {7, 77, 17};
-        int[] a2 = {10, 29};
+        int[] a1 = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+        int[] a2 = {7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2};
+        int[] a3 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
 
-        System.out.println(solution.solution(a1));
-        System.out.println(solution.solution(a2));
+        System.out.println(solution.solution(a1, "right"));
+        System.out.println(solution.solution(a2, "left"));
+        System.out.println(solution.solution(a3, "right"));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,39 @@
-import java.util.Arrays;
-
 class Solution {
 
-    public int solution(int[] array) {
-        int answer = 0;
-        String str = Arrays.toString(array);
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '7') {
-                answer++;
+    public String solution(int[] numbers, String hand) {
+        String answer = "";
+        int left = 10;
+        int right = 12;
+
+        for (int tmp : numbers) {
+            if (tmp == 1 || tmp == 4 || tmp == 7) {
+                answer += "L";
+                left = tmp;
+            } else if (tmp == 3 || tmp == 6 || tmp == 9) {
+                answer += "R";
+                right = tmp;
+            } else {
+                if (tmp == 0) {
+                    tmp = 11;
+                }
+                int leftdist = Math.abs(tmp - left) / 3 + Math.abs(tmp - left) % 3;
+                int rightdist = Math.abs(tmp - right) / 3 + Math.abs(tmp - right) % 3;
+
+                if (leftdist < rightdist) {
+                    answer += "L";
+                    left = tmp;
+                } else if (leftdist > rightdist) {
+                    answer += "R";
+                    right = tmp;
+                } else {
+                    if (hand.equals("left")) {
+                        answer += "L";
+                        left = tmp;
+                    } else {
+                        answer += "R";
+                        right = tmp;
+                    }
+                }
             }
         }
         return answer;
