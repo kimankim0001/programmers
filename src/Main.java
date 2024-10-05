dd2a46aeb55059f9c4bca145c300f1326e7b4b28
kinam <maltize9292@gmail.com>
2024-10-06 00:05:46 +0900

    24-10-06 컨트롤 제트

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,10 @@ public class Main {
 
         Solution solution = new Solution();
 
-        int[] a1 = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
-        int[] a2 = {7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2};
-        int[] a3 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-
-        System.out.println(solution.solution(a1, "right"));
-        System.out.println(solution.solution(a2, "left"));
-        System.out.println(solution.solution(a3, "right"));
+        System.out.println(solution.solution("1 2 Z 3"));
+        System.out.println(solution.solution("10 20 30 40"));
+        System.out.println(solution.solution("10 Z 20 Z 1"));
+        System.out.println(solution.solution("10 Z 20 Z"));
+        System.out.println(solution.solution("-1 -2 -3 Z"));
     }
 }
