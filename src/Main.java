8fd6f31daaa313cad961f1b9a2e45a2469dbc6ae
kinam <maltize9292@gmail.com>
2024-10-08 12:28:57 +0900

    24-10-08 공백으로 구분하기 1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("Progra21Sremm3", 6, 12));
-        System.out.println(solution.solution("Stanley1yelnatS", 4, 10));
+        System.out.println(Arrays.toString(solution.solution("i love you")));
+        System.out.println(Arrays.toString(solution.solution("programmers")));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,8 +1,6 @@
 class Solution {
 
-    public String solution(String my_string, int s, int e) {
-        StringBuilder answer = new StringBuilder(my_string.substring(s, e + 1));
-        answer.reverse();
-        return my_string.substring(0, s) + answer + my_string.substring(e + 1);
+    public String[] solution(String my_string) {
+        return my_string.split(" ");
     }
 }
