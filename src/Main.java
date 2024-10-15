f8261eb26cd45024a5c0e179b632a4927cb3d233
kinam <maltize9292@gmail.com>
2024-10-15 14:06:57 +0900

    24-10-15 n 번째 원소부터

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
 
-        int[] a1 = {1, 2, 3, 4, 5};
-        int[] a2 = {15, 98, 23, 2, 15};
+        int[] a1 = {2, 1, 6};
+        int[] a2 = {5, 2, 1, 7, 5};
 
-        System.out.println(solution.solution(a1, 3));
-        System.out.println(solution.solution(a2, 20));
+        System.out.println(Arrays.toString(solution.solution(a1, 3)));
+        System.out.println(Arrays.toString(solution.solution(a2, 2)));
+
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,12 @@
 class Solution {
 
-    public int solution(int[] num_list, int n) {
-        int answer = 0;
-        for (int i = 0; i < num_list.length; i++) {
-            if (num_list[i] == n) {
-                answer = 1;
-            }
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[(num_list.length) - (n - 1)];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = num_list[(n - 1) + i];
         }
+
         return answer;
     }
 }
