995d2dd99b8aa9b4143f62b6ef20de1775b55e5e
kinam <maltize9292@gmail.com>
2024-10-20 14:24:29 +0900

    24-10-20 특정 문자 제거하기

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,10 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
 
-        int[] s1 = {1, 5, 2};
-        int[] s2 = {1, 5, 2};
-
-        int[] n1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int[] n2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-        System.out.println(Arrays.toString(solution.solution(3, s1, n1)));
-        System.out.println(Arrays.toString(solution.solution(4, s2, n2)));
+        System.out.println(solution.solution("abcdef", "f"));
+        System.out.println(solution.solution("BCBdbe", "B"));
     }
 }
