c7d432ff729aaa3f773227aa8fe2d6f2134e15a0
kinam <maltize9292@gmail.com>
2024-10-23 16:35:27 +0900

    24-10-23 배열 만들기 5

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,8 @@ public class Main {
 
         Solution solution = new Solution();
 
-        int[] e1 = {3, 76, 24};
-        int[] e2 = {1, 2, 3, 4, 5, 6, 7};
-        int[] e3 = {30, 10, 23, 6, 100};
+        String[] i1 = {"0123456789", "9876543210", "9999999999999"};
 
-        System.out.println(Arrays.toString(solution.solution(e1)));
-        System.out.println(Arrays.toString(solution.solution(e2)));
-        System.out.println(Arrays.toString(solution.solution(e3)));
+        System.out.println(Arrays.toString(solution.solution(i1, 50000, 5, 5)));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,18 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
 
-    public int[] solution(int[] emergency) {
-        int[] asc = emergency.clone();
-        int[] answer = new int[emergency.length];
-        Arrays.sort(asc);
-        for (int i = 0; i < asc.length; i++) {
-            for (int j = 0; j < emergency.length; j++) {
-                if (asc[i] == emergency[j]) {
-                    answer[j] = emergency.length - i;
-                }
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        List<Integer> result = new ArrayList<>();
+
+        for (String str : intStrs) {
+            int i = Integer.parseInt(str.substring(s, s + l));
+            if (i > k) {
+                result.add(i);
             }
         }
-        return answer;
+
+        return result.stream().mapToInt(i -> i).toArray();
     }
 }
