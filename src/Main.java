25e35d20a7253db4d3c916fce10bad268bb5ed24
kinam <maltize9292@gmail.com>
2024-10-28 12:28:27 +0900

    24-10-28 실패율

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(144));
-        System.out.println(solution.solution(976));
+        int[] s1 = {2, 1, 2, 6, 2, 4, 3, 3};
+        int[] s2 = {4, 4, 4, 4, 4};
+
+        System.out.println(Arrays.toString(solution.solution(5, s1)));
+        System.out.println(Arrays.toString(solution.solution(4, s2)));
     }
 }
