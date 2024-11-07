e2481bdff313a5ab5e9223aadba0cf054d6c3d94
kinam <maltize9292@gmail.com>
2024-11-07 21:36:16 +0900

    24-11-07 편지

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,7 @@ public class Main {
 
         Solution solution = new Solution();
 
-        int[][] b1 = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}};
-        int[][] b2 = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 1, 0}, {0, 0, 0, 0, 0}};
-        int[][] b3 = {{1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1},
-            {1, 1, 1, 1, 1, 1}};
-
-        System.out.println(solution.solution(b1));
-        System.out.println(solution.solution(b2));
-        System.out.println(solution.solution(b3));
+        System.out.println(solution.solution("happy birthday!"));
+        System.out.println(solution.solution("I love you~"));
     }
 }
