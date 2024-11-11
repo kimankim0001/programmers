8ed7944140ba03a4b67f272b6066948404567c02
kinam <maltize9292@gmail.com>
2024-11-11 21:42:23 +0900

    24-11-11 n개 간격의 원소들

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
 
-        int[][] a1 = {{5, 192, 33}, {192, 72, 95}, {33, 95, 999}};
-        int[][] a2 = {{19, 498, 258, 587}, {63, 93, 7, 754}, {258, 7, 1000, 723}, {587, 754, 723, 81}};
+        int[] n1 = {4, 2, 6, 1, 7, 6};
+        int[] n2 = {4, 2, 6, 1, 7, 6};
 
-        System.out.println(solution.solution(a1));
-        System.out.println(solution.solution(a2));
+        System.out.println(Arrays.toString(solution.solution(n1, 2)));
+        System.out.println(Arrays.toString(solution.solution(n2, 4)));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,17 @@
 class Solution {
 
-    public int solution(int[][] arr) {
-        int n = arr.length;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (arr[i][j] != arr[j][i]) {
-                    return 0;
-                }
-            }
+    public int[] solution(int[] num_list, int n) {
+        int[] answer;
+        if (num_list.length % n == 0) {
+            answer = new int[num_list.length / n];
+        } else {
+            answer = new int[num_list.length / n + 1];
         }
-        return 1;
+
+        int index = 0;
+        for (int i = 0; i < num_list.length; i += n) {
+            answer[index++] = num_list[i];
+        }
+        return answer;
     }
 }
