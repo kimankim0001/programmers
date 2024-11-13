f0e93e8a6873db31fd436a9b316326297c4dd907
kinam <maltize9292@gmail.com>
2024-11-13 21:13:19 +0900

    24-11-13 나머지 구하기

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,7 @@ public class Main {
 
         Solution solution = new Solution();
 
-        int[] i1 = {16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7};
-        int[] i2 = {1, 2, 0, 0, 3};
-
-        System.out.println(solution.solution("cvsgiorszzzmrpaqpe", i1));
-        System.out.println(solution.solution("zpiaz", i2));
+        System.out.println(solution.solution(3, 2));
+        System.out.println(solution.solution(10, 5));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,8 @@
 class Solution {
 
-    public String solution(String my_string, int[] index_list) {
-        String answer = "";
-
-        for (int i = 0; i < index_list.length; i++) {
-            answer += my_string.charAt(index_list[i]);
-        }
-
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        answer = num1 % num2;
         return answer;
     }
 }
