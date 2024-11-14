b4b1f4bd386d5a5107e7d2db6650d54a6100d294
kinam <maltize9292@gmail.com>
2024-11-14 20:54:33 +0900

    24-11-14 가장 많이 받은 선물

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,16 @@ public class Main {
 
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(3, 2));
-        System.out.println(solution.solution(10, 5));
+        String[] f1 = {"muzi", "ryan", "frodo", "neo"};
+        String[] f2 = {"joy", "brad", "alessandro", "conan", "david"};
+        String[] f3 = {"a", "b", "c"};
+
+        String[] g1 = {"muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi", "frodo ryan", "neo muzi"};
+        String[] g2 = {"alessandro brad", "alessandro joy", "alessandro conan", "david alessandro", "alessandro david"};
+        String[] g3 = {"a b", "b a", "c a", "a c", "a c", "c a"};
+
+        System.out.println(solution.solution(f1, g1));
+        System.out.println(solution.solution(f2, g2));
+        System.out.println(solution.solution(f3, g3));
     }
 }
