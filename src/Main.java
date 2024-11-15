935e6dda8a15c22063bee0669e255f9121afb90c
kinam <maltize9292@gmail.com>
2024-11-15 22:31:41 +0900

    24-11-15 뒤에서 5등 위로

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
 
-        String[] f1 = {"muzi", "ryan", "frodo", "neo"};
-        String[] f2 = {"joy", "brad", "alessandro", "conan", "david"};
-        String[] f3 = {"a", "b", "c"};
+        int[] nl = {12, 4, 15, 46, 38, 1, 14, 56, 32, 10};
 
-        String[] g1 = {"muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi", "frodo ryan", "neo muzi"};
-        String[] g2 = {"alessandro brad", "alessandro joy", "alessandro conan", "david alessandro", "alessandro david"};
-        String[] g3 = {"a b", "b a", "c a", "a c", "a c", "c a"};
-
-        System.out.println(solution.solution(f1, g1));
-        System.out.println(solution.solution(f2, g2));
-        System.out.println(solution.solution(f3, g3));
+        System.out.println(Arrays.toString(solution.solution(nl)));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,42 +1,13 @@
-import java.util.HashMap;
+import java.util.Arrays;
 
 class Solution {
 
-    public int solution(String[] friends, String[] gifts) {
-        int answer = 0;
-        int friendsLenght = friends.length;
-        HashMap<String, Integer> dic = new HashMap<>();
-        int[] giftDegree = new int[friendsLenght];
-        int[][] giftGraph = new int[friendsLenght][friendsLenght];
-
-        for (int i = 0; i < friendsLenght; i++) {
-            dic.put(friends[i], i);
-        }
-
-        for (String gift : gifts) {
-            String[] giftName = gift.split(" ");
-            giftDegree[dic.get(giftName[0])]++;
-            giftDegree[dic.get(giftName[1])]--;
-            giftGraph[dic.get(giftName[0])][dic.get(giftName[1])]++;
-        }
-
-        for (int i = 0; i < friendsLenght; i++) {
-            int num = 0;
-            for (int j = 0; j < friendsLenght; j++) {
-                if (i == j) {
-                    continue;
-                }
-
-                if (giftGraph[i][j] > giftGraph[j][i] ||
-                    (giftGraph[i][j] == giftGraph[j][i] && giftDegree[i] > giftDegree[j])) {
-                    num++;
-                }
-            }
-
-            if (answer < num) {
-                answer = num;
-            }
-
+    public int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+        int n = 5;
+        int[] answer = new int[num_list.length - n];
+        for (int i = n; i < num_list.length; i++) {
+            answer[i - n] = num_list[i];
         }
 
         return answer;
