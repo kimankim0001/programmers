283cfa78099591a0932b12cd3695cc0d77737ba5
kinam <maltize9292@gmail.com>
2024-11-16 13:09:51 +0900

    24-11-16 부분 문자열인지 확인하기

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
 
-        int[] nl = {12, 4, 15, 46, 38, 1, 14, 56, 32, 10};
-
-        System.out.println(Arrays.toString(solution.solution(nl)));
+        System.out.println(solution.solution("banana", "ana"));
+        System.out.println(solution.solution("banana", "wxyz"));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,10 @@
-import java.util.Arrays;
-
 class Solution {
 
-    public int[] solution(int[] num_list) {
-        Arrays.sort(num_list);
-        int n = 5;
-        int[] answer = new int[num_list.length - n];
-        for (int i = n; i < num_list.length; i++) {
-            answer[i - n] = num_list[i];
+    public int solution(String my_string, String target) {
+        int answer = 0;
+        if (my_string.contains(target)) {
+            answer = 1;
         }
-
         return answer;
     }
 }
