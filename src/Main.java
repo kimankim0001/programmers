54655125366eb60727d48c52f9dacab07156f080
kinam <maltize9292@gmail.com>
2024-11-18 06:47:41 +0900

    24-11-18 소인수분해

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
 
-        int[] n1 = {12, 4, 15, 46, 38, -2, 15};
-        int[] n2 = {13, 22, 53, 24, 15, 6};
-
-        System.out.println(solution.solution(n1));
-        System.out.println(solution.solution(n2));
+        System.out.println(Arrays.toString(solution.solution(12)));
+        System.out.println(Arrays.toString(solution.solution(17)));
+        System.out.println(Arrays.toString(solution.solution(420)));
     }
 }
