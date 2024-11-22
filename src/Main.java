d586ffc68bb29ef0039cdb2a996969faf5710157
kinam <maltize9292@gmail.com>
2024-11-22 14:06:13 +0900

    24-11-22 나이 계산

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        String age_type = sc.next();
+        int answer = 0;
 
-        Solution solution = new Solution();
+        if (age_type.equals("Korea")) {
+            answer = 2030 - year + 1;
+        } else if (age_type.equals("Year")) {
+            answer = 2030 - year;
+        }
 
-        System.out.println(solution.solution("people"));
-        System.out.println(solution.solution("We are the world"));
+        System.out.println(answer);
     }
 }
