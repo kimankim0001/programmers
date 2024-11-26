83d8642813b74ec000315ec4040ca46123abead1
kinam <maltize9292@gmail.com>
2024-11-27 05:56:08 +0900

    24-11-27 콜라츠 수열 만들기

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(2, 2, 2, 2));
-        System.out.println(solution.solution(4, 1, 4, 4));
-        System.out.println(solution.solution(6, 3, 3, 6));
-        System.out.println(solution.solution(2, 5, 2, 6));
-        System.out.println(solution.solution(6, 4, 2, 5));
+        System.out.println(Arrays.toString(solution.solution(10)));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,54 +1,24 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
 
-    public int solution(int a, int b, int c, int d) {
-        int answer = 0;
-        int[] nums = new int[4];
-        nums[0] = a;
-        nums[1] = b;
-        nums[2] = c;
-        nums[3] = d;
+    public int[] solution(int n) {
+        List<Integer> sequence = new ArrayList<>();
+        sequence.add(n);
 
-        int multi = 1;
-
-        Arrays.sort(nums);
-        int[] dice = new int[6];
-        for (int i = 0; i < 4; i++) {
-            dice[nums[i] - 1]++;
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = 3 * n + 1;
+            }
+            sequence.add(n);
         }
 
-        for (int i = 0; i < 6; i++) {
-            if (dice[i] == 4) {
-                answer = 1111 * (i + 1);
-                break;
-            } else if (dice[i] == 3) {
-                for (int j = 0; j < 6; j++) {
-                    if (dice[j] == 1) {
-                        answer = (10 * (i + 1) + (j + 1)) * (10 * (i + 1) + (j + 1));
-                        break;
-                    }
-                }
-            } else if (dice[i] == 2) {
-                for (int j = 0; j < 6; j++) {
-                    if (dice[j] == 2) {
-                        if (i == j) {
-                            continue;
-                        } else {
-                            answer = (i + 1 + j + 1) * ((i + 1) - (j + 1));
-                            break;
-                        }
-                    } else if (dice[j] == 1) {
-                        multi = multi * (j + 1);
-                    }
-                }
-                if (multi != 1) {
-                    answer = multi;
-                }
-            }
-            if (nums[0] != nums[1] && nums[1] != nums[2] && nums[2] != nums[3]) {
-                answer = nums[0];
-            }
+        int[] answer = new int[sequence.size()];
+        for (int i = 0; i < sequence.size(); i++) {
+            answer[i] = sequence.get(i);
         }
         return answer;
     }
