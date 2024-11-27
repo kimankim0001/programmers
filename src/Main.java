100f4850a2c36a8aa348f961166f7f1977c86123
kinam <maltize9292@gmail.com>
2024-11-28 05:32:59 +0900

    24-11-28 rny_string

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(Arrays.toString(solution.solution(10)));
+        System.out.println(solution.solution("masterpiece"));
+        System.out.println(solution.solution("programmers"));
+        System.out.println(solution.solution("jerry"));
+        System.out.println(solution.solution("burn"));
     }
 }
