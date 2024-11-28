4246050ce6c287175ac6863466ea9c34b3bc41ba
kinam <maltize9292@gmail.com>
2024-11-29 05:17:15 +0900

    24-11-29 홀수 vs 짝수

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("masterpiece"));
-        System.out.println(solution.solution("programmers"));
-        System.out.println(solution.solution("jerry"));
-        System.out.println(solution.solution("burn"));
+        int[] n1 = {4, 2, 6, 1, 7, 6};
+        int[] n2 = {-1, 2, 5, 6, 3};
+
+        System.out.println(solution.solution(n1));
+        System.out.println(solution.solution(n2));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,17 @@
 class Solution {
 
-    public String solution(String rny_string) {
-        String answer = rny_string.replaceAll("m", "rn");
-        return answer;
+    public int solution(int[] num_list) {
+        int oddSum = 0;
+        int evenSum = 0;
+
+        for (int i = 0; i < num_list.length; i++) {
+            if (i % 2 == 0) { // 짝수 인덱스
+                evenSum += num_list[i];
+            } else { // 홀수 인덱스
+                oddSum += num_list[i];
+            }
+        }
+
+        return (oddSum > evenSum) ? oddSum : evenSum;
     }
 }
