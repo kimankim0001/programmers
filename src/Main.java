7a61179f333571c6041fc9b06c4b2efddb0ca7a2
kinam <maltize9292@gmail.com>
2024-11-30 06:56:14 +0900

    24-11-30 특수문자 출력하기

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
 
-        int[] n1 = {4, 2, 6, 1, 7, 6};
-        int[] n2 = {-1, 2, 5, 6, 3};
-
-        System.out.println(solution.solution(n1));
-        System.out.println(solution.solution(n2));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,17 +1,6 @@
-class Solution {
+public class Solution {
 
-    public int solution(int[] num_list) {
-        int oddSum = 0;
-        int evenSum = 0;
-
-        for (int i = 0; i < num_list.length; i++) {
-            if (i % 2 == 0) { // 짝수 인덱스
-                evenSum += num_list[i];
-            } else { // 홀수 인덱스
-                oddSum += num_list[i];
-            }
-        }
-
-        return (oddSum > evenSum) ? oddSum : evenSum;
+    public static void main(String[] args) {
+        System.out.print("!@#$%^&*(\\'\"<>?:;");
     }
 }
