5d734db6e9551fbe6af99f0370ab81c4431c2c7d
kinam <maltize9292@gmail.com>
2024-12-01 17:28:41 +0900

    24-12-01 카운트 다운

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Solution s1 = new Solution();
+
+        System.out.println(Arrays.toString(s1.solution(10, 3)));
     }
 }
