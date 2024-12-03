fbd8957a91e97f8811bf3b08b00d714447f37672
kinam <maltize9292@gmail.com>
2024-12-04 07:21:09 +0900

    24-12-04 종이 자르기

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
 
         Solution s1 = new Solution();
 
-        int[] a1 = {1, 2, 3, 4, 5, 6};
-        int[] a2 = {58, 172, 746, 89};
-
-        System.out.println(Arrays.toString(s1.solution(a1)));
-        System.out.println(Arrays.toString(s1.solution(a2)));
+        System.out.println(s1.solution(2, 2));
+        System.out.println(s1.solution(2, 5));
+        System.out.println(s1.solution(1, 1));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,7 @@
-import java.util.Arrays;
-
 class Solution {
 
-    public int[] solution(int[] arr) {
-        int n = arr.length;
-        int targetLength = 1;
-
-        while (targetLength < n) {
-            targetLength *= 2;
-        }
-
-        int[] answer = new int[targetLength];
-        Arrays.fill(answer, 0);
-
-        System.arraycopy(arr, 0, answer, 0, n);
-
+    public int solution(int M, int N) {
+        int answer = M * N - 1;
         return answer;
     }
 }
