bf0b7dbf6b27eb4dac2cdc290fcf22041767caca
kinam <maltize9292@gmail.com>
2024-12-20 00:39:28 +0900

    24-12-20 간단한 논리 연산

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,12 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        Solution s1 = new Solution();
-        
-        int[] n1 = new int[]{0,1,1,2,2,3};
-        int[] n2 = new int[]{0,1,1,1,1};
+        Solution s1 = new Solution();        
 
-        System.out.println(Arrays.toString(s1.solution(n1,3)));
-        System.out.println(Arrays.toString(s1.solution(n2,4)));
+
+        System.out.println(s1.solution(false,true,true,true));
+        System.out.println(s1.solution(true,false,false,false));
         
     }
 }
