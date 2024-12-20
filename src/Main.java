9af231db22a6502520ce541baf997a857e945c11
kinam <maltize9292@gmail.com>
2024-12-21 00:31:28 +0900

    24-12-21 정사각형으로 만들기

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Solution s1 = new Solution();        
+        Solution s1 = new Solution();
+        
+        int[][] arr1 = new int[][] {{572,22,37},{287,726,384},{85,137,292},{487,13,876}};
+        int[][] arr2 = new int[][] {{57,192,534,2},{9,345,192,999}};
+        int[][] arr3 = new int[][] {{1,2},{3,4}};
 
 
-        System.out.println(s1.solution(false,true,true,true));
-        System.out.println(s1.solution(true,false,false,false));
+        System.out.println(Arrays.deepToString(s1.solution(arr1)));
+        System.out.println(Arrays.deepToString(s1.solution(arr2)));
+        System.out.println(Arrays.deepToString(s1.solution(arr3)));
         
     }
 }
