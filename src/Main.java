e2abdbf753570d600c002f0e9001345cd0938041
kinam <maltize9292@gmail.com>
2024-12-23 19:24:48 +0900

    24-12-23 문자열을 정수로 변환하기

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,12 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
 
         Solution s1 = new Solution();
+                        
         
-        int[] arr1 = new int[]{3,2,4,1,3};
-        boolean[] flag1 = new boolean[]{true, false, true, false, false};
-
-
-        System.out.println(Arrays.toString(s1.solution(arr1,flag1)));
-        
+        System.out.println(s1.solution("10"));
+        System.out.println(s1.solution("8542"));    
         
     }
 }
