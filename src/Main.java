91ba0c915f97227d77ec6f8f8de0f3c30502213e
kinam <maltize9292@gmail.com>
2024-12-24 10:38:12 +0900

    24-12-24 배열 비교하기

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,18 @@ public class Main {
 
         Solution s1 = new Solution();
                         
+        int[] a1 = new int[]{49,13};
+        int[] a2 = new int[]{100,17,84,1};
+        int[] a3 = new int[]{1,2,3,4,5};
         
-        System.out.println(s1.solution("10"));
-        System.out.println(s1.solution("8542"));    
+        int[] b1 = new int[]{70,11,2};
+        int[] b2 = new int[]{55,12,65,36};
+        int[] b3 = new int[]{3,3,3,3,3};        
+        
+        
+        System.out.println(s1.solution(a1,b1));
+        System.out.println(s1.solution(a2,b2));
+        System.out.println(s1.solution(a3,b3));
         
     }
 }
