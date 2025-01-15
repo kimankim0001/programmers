1dd55be82205d9493649ea0364a734020771cda9
kinam <maltize9292@gmail.com>
2025-01-16 06:54:24 +0900

    25-01-16 문자열로 변환

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         Solution s1 = new Solution();
                                
-        System.out.println(s1.solution("aAb1B2cC34oOp"));
-        System.out.println(s1.solution("1a2b3c4d123")); 
+        System.out.println(s1.solution(123));
+        System.out.println(s1.solution(2573)); 
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,6 @@
 class Solution {
-    public int solution(String my_string) {
-        int answer = 0;
-        
-        my_string = my_string.replaceAll("[a-zA-Z]", "");
-        String[] string = my_string.split("");
-        
-        for(String i : string) {
-            answer += Integer.parseInt(i);
-        }
-        
+    public String solution(int n) {
+        String answer = Integer.toString(n);
         return answer;
     }
 }
