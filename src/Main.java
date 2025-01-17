de2846eab7f8afe2861405e8019c7a448f3eee46
kinam <maltize9292@gmail.com>
2025-01-18 07:13:19 +0900

    25-01-18 순서 바꾸기

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Solution s1 = new Solution();
+        
+        int[] a1 = {2,1,6};
+        int[] a2 = {5,2,1,7,5};
                                
-        System.out.println(s1.solution("582","734"));
-        System.out.println(s1.solution("18446744073709551615","287346502836570928366"));
-        System.out.println(s1.solution("0","0"));
+        System.out.println(Arrays.toString(s1.solution(a1,1)));
+        System.out.println(Arrays.toString(s1.solution(a2,3)));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,11 +1,16 @@
-import java.math.BigInteger;
 class Solution {
-    public String solution(String a, String b) {
-        BigInteger big1 = new BigInteger(a);
-        BigInteger big2 = new BigInteger(b);
-        
-        BigInteger sum = big1.add(big2);
-        String answer = sum.toString();
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length];
+        int index = 0;
+
+        for (int i = n; i < num_list.length; i++) {
+            answer[index++] = num_list[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            answer[index++] = num_list[i];
+        }
+
         return answer;
     }
 }
