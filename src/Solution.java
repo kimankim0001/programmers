ba2e5684978f0e0e9a2e715ff30f481514dbfd53
kinam <maltize9292@gmail.com>
2025-01-22 19:30:02 +0900

    25-01-22

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,11 @@
-import java.util.*;
-
 class Solution {
-    public String solution(String my_string, int m, int c) {
-        List<String> result = new ArrayList<>();
-        for (int i = 0; i < my_string.length(); i += m) {
-            String sub = my_string.substring(i, i + m);
-            result.add(sub);
-        }
+    public int solution(int a, int b, boolean flag) {
+        int answer = 0;
         
-        String answer = "";
-        for (String str : result) {
-            answer += str.substring(c-1, c);
+        if (!flag) {
+            return a - b;
+        } else {
+            return a + b;
         }
-        
-        return answer;
     }
 }
