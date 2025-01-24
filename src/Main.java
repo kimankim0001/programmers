e73c7f52937ed9a326c96214acfe73d12381b70f
kinam <maltize9292@gmail.com>
2025-01-24 13:42:17 +0900

    25-01-24

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Solution s1 = new Solution();
 
+        int[] arr1 = {5, 1, 4};
                                
-        System.out.println(s1.solution("123456789"));        
+        System.out.println(Arrays.toString(s1.solution(arr1)));        
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,9 +1,19 @@
-class Solution {
-    public int solution(String num_str) {
-        int answer = 0;
+import java.util.ArrayList;
+import java.util.List;
 
-        for(int i=0; i<num_str.length(); i++){
-            answer += Integer.parseInt(num_str.substring(i, i+1));
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int num : arr) {
+            for (int i = 0; i < num; i++) {
+                result.add(num);
+            }
+        }
+
+        int[] answer = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
         }
 
         return answer;
