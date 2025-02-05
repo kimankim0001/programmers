d66447f002d66f68e004537a7ddba68ce02799f9
kinam <maltize9292@gmail.com>
2025-02-05 19:48:12 +0900

    수 조작하기 2

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ public class Main {
 
         Solution s1 = new Solution();
 
-        String[] arr1 = {"progressive", "hamburger", "hammer", "ahocorasick"};
-        int[][] arr2 = {{0,4},{1,2},{3,5},{7,7}};
+        int[] arr1 = {0, 1, 0, 10, 0, 1, 0, 10, 0, -1, -2, -1};
+//        int[][] arr2 = {{0,4},{1,2},{3,5},{7,7}};
                                
-        System.out.println(s1.solution(arr1, arr2));        
+        System.out.println(s1.solution(arr1));        
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,12 @@
 class Solution {
-    public String solution(String[] my_strings, int[][] parts) {
-        String answer = "";
-        
-        for(int i=0; i<my_strings.length; i++){
-            String str = my_strings[i];
-            
-            answer += str.substring(parts[i][0], parts[i][1]+1);
-        }
-        
-        return answer;
-    }
+    public String solution(int[] numLog) {
+		String answer = "";
+		for (int i = 1; i < numLog.length; i++) {
+                 if((numLog[i] - numLog[i-1]) == 1) answer += "w";
+			else if((numLog[i] - numLog[i-1]) == -1) answer += "s";
+			else if((numLog[i] - numLog[i-1]) == 10) answer += "d";
+			else if((numLog[i] - numLog[i-1]) == -10) answer += "a";
+		}
+		return answer;
+	}
 }
