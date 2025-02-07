772a3b5e26e0e0e0d12431a1f71836ad834eb64f
kinam <maltize9292@gmail.com>
2025-02-07 19:47:10 +0900

    조건에 맞게 수열 변환하기 3

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ public class Main {
 
         Solution s1 = new Solution();
 
-//        int[] arr1 = {0, 1, 0, 10, 0, 1, 0, 10, 0, -1, -2, -1};
+        int[] arr1 = {1, 2, 3, 100, 99, 98};
 //        int[][] arr2 = {{0,4},{1,2},{3,5},{7,7}};
                                
-        System.out.println(s1.solution("43 + 12"));        
+        System.out.println(Arrays.toString(s1.solution(arr1,3)));        
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,23 +1,15 @@
 class Solution {
-    public int solution(String binomial) {
-        int answer = 0;
-        
-        // 이항식을 공백을 기준으로 분리
-        String[] parts = binomial.split(" ");
-        
-        int a = Integer.parseInt(parts[0]);
-        String op = parts[1];
-        int b = Integer.parseInt(parts[2]);
-        
-        // 연산자에 따라 계산 수행
-        if (op.equals("+")) {
-            answer = a + b;
-        } else if (op.equals("-")) {
-            answer = a - b;
-        } else if (op.equals("*")) {
-            answer = a * b;
+    public int[] solution(int[] arr, int k) {
+        if(k%2==0){
+            for(int i=0; i<arr.length;i++){
+                arr[i] += k;   
+            }
         }
-        
-        return answer;
+        else{
+            for(int i=0; i<arr.length;i++){
+                arr[i] *= k;   
+            }
+        }
+        return arr;
     }
 }
