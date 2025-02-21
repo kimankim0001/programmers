c1d2b016e1af6b41a20eeacb07026e7d645bec7c
kinam <maltize9292@gmail.com>
2025-02-21 19:25:01 +0900

    왼쪽 오른쪽

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,9 @@ public class Main {
 //        int[] arr2 = {4, 1, 2};        
         int[][] arr3 = {{0,4,1},{0,3,2},{0,3,3}};
         
-        String[] str1 = {"a","bc","d","efg","hi"};
+        String[] str1 = {"u", "u", "l", "r"};
                                
-        System.out.println(s1.solution(str1));
-//        System.out.println(Arrays.toString(s1.solution(arr1,arr3)));
+//        System.out.println(s1.solution(str1));
+        System.out.println(Arrays.toString(s1.solution(str1)));
     }
 }
