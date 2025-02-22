2035dc57046b52909aa5256258347ae76722550a
kinam <maltize9292@gmail.com>
2025-02-22 19:35:42 +0900

    프로그래머스

--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,12 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
-class Solution {
-    public String[] solution(String[] str_list) {
-        String[] answer = {};
-        for (int i = 0; i < str_list.length;i++) {
-            if (str_list[i].equals("l")) {
-                return Arrays.copyOfRange(str_list, 0, i);
-            } else if (str_list[i].equals("r")) {
-                return Arrays.copyOfRange(str_list, i + 1, str_list.length);
-            }
-        }
-        return answer;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
     }
 }
