823c50de67b681f0a755d2802a34b73b4a4f10ae
kinam <maltize9292@gmail.com>
2025-02-23 19:42:45 +0900

    프로그래머스

--- src/Solution.java
+++ src/Solution.java
@@ -3,10 +3,18 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        System.out.println("a = "+a);
-        System.out.println("b = "+b);
+        String a = sc.next();
+        
+        String answer = "";
+        
+        for(int i = 0; i<a.length(); i++){
+            char c = a.charAt(i);
+            if(Character.isUpperCase(c)){
+                answer += Character.toLowerCase(c);
+            }else{
+                answer += Character.toUpperCase(c);
+            }
+        }
+       System.out.println(answer);
     }
 }
