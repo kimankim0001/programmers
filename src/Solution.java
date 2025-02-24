f59741d9d46f6ba5c680789ed379ef46ec797c92
kinam <maltize9292@gmail.com>
2025-02-24 19:22:12 +0900

    프로그래머스

--- src/Solution.java
+++ src/Solution.java
@@ -4,17 +4,8 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
+        String b = sc.next();
         
-        String answer = "";
-        
-        for(int i = 0; i<a.length(); i++){
-            char c = a.charAt(i);
-            if(Character.isUpperCase(c)){
-                answer += Character.toLowerCase(c);
-            }else{
-                answer += Character.toUpperCase(c);
-            }
-        }
-       System.out.println(answer);
+        System.out.println(a + b);
     }
 }
