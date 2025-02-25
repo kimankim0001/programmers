29bf4a822f0abe03c406e6503bb24f01838a5633
kinam <maltize9292@gmail.com>
2025-02-25 19:54:10 +0900

    프로그래머스

--- src/Solution.java
+++ src/Solution.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        String b = sc.next();
+        String str = sc.next();
         
-        System.out.println(a + b);
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.substring(i, i + 1));
+        }
     }
 }
