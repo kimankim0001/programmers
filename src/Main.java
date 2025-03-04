507063b73669c97b01e0c9b7bb78fdc2c4525c14
kinam <maltize9292@gmail.com>
2025-03-04 19:25:40 +0900

    프로그래머스

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,15 @@ public class Main {
 
         Solution s1 = new Solution();
 
-        int[] arr1 = {0, 1, 2, 4, 3};
+        int[] arr1 = {3, 7, 2, 5, 4, 6, 1};
 //        int[] arr2 = {4, 1, 2};        
         int[][] arr3 = {{0,4,1},{0,3,2},{0,3,3}};
         
-        boolean[] boo1 = {true, false, false, true, true};
+        boolean[] boo1 = {false, true, true, true, true, false, false};
         
         String[] str1 = {"u", "u", "l", "r"};
                                
 //        System.out.println(s1.solution(str1));
-        System.out.println(s1.solution(100));
+        System.out.println(s1.solution(arr1, boo1));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,11 +1,24 @@
-class Solution {
-    public int solution(int chicken) {
-        int service = 0;
+import java.util.*;
 
-        while (chicken >= 10) {
-            service += chicken / 10;
-            chicken = (chicken / 10) + (chicken % 10);
+class Solution {
+    public int solution(int[] rank, boolean[] attendance) {
+        int answer = 0;
+        Map<Integer, Integer> attendMap = new HashMap<>();
+        for(int i = 0; i < attendance.length; i++) {
+            if(attendance[i] == true) {
+                attendMap.put(rank[i], i);
+            }
         }
-        return service;
+        List<Integer> keySet = new ArrayList<>(attendMap.keySet());
+        Collections.sort(keySet);
+        int[] inThird = keySet.stream().mapToInt(Integer::intValue).toArray();
+
+        int a = attendMap.get(inThird[0]);
+        int b = attendMap.get(inThird[1]);
+        int c = attendMap.get(inThird[2]);
+
+        answer = (10000 * a) + (100 * b) + c;
+        
+        return answer;
     }
 }
