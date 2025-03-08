38404211c4d904d1c48cf9da8e34284fe87fdd60
kinam <maltize9292@gmail.com>
2025-03-08 19:05:32 +0900

    프로그래머스

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,15 @@ public class Main {
 
         Solution s1 = new Solution();
 
-        int[] arr1 = {1, 2, 1, 4, 5, 2, 9};
+        int[] arr1 = {1, 2, 3, 4, 5};
         int[][] arr3 = {{0,3},{1,2},{1,4}};
         
         boolean[] boo1 = {false, true, true, true, true, false, false};
         
         String[] str1 = {"u", "u", "l", "r"};
                                
-        System.out.println(s1.solution("hello",3));
+//        System.out.println(s1.solution("hello",3));
 //        System.out.println(s1.solution("hello", "ohell"));
-//        System.out.println(Arrays.toString(s1.solution(arr1)));
+        System.out.println(Arrays.toString(s1.solution(arr1,1,3)));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,11 @@
 class Solution {
-    public String solution(String my_string, int n) {
-        String answer = "";
-        
-        for(int i = 0; i < my_string.length(); i++) {
-            for(int j = 0; j < n; j++) {
-                answer += my_string.charAt(i);
-            }
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = new int[num2 - num1 + 1];
+
+        for (int i = num1; i <= num2; i++) {
+            answer[i -num1] = numbers[i];
         }
+
         return answer;
     }
 }
