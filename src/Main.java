f8897f937d630ecb05ab845b53842e5a243a39dc
kinam <maltize9292@gmail.com>
2025-03-11 18:49:08 +0900

    프로그래머스

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,15 @@ public class Main {
 
         Solution s1 = new Solution();
 
-        int[] arr1 = {1, 2};
-        int[][] arr3 = {{0,3},{1,2},{1,4}};
+        int[] arr1 = {0, 1, 2, 4, 3};
+        int[][] arr3 = {{0, 4, 2},{0, 3, 2},{0, 2, 2}};
         
         boolean[] boo1 = {false, true, true, true, true, false, false};
         
         String[] str1 = {"u", "u", "l", "r"};
                                
-        System.out.println(s1.solution(arr1));
+//        System.out.println(s1.solution(arr1));
 //        System.out.println(s1.solution("hello", "ohell"));
-//        System.out.println(Arrays.toString(s1.solution(3,12)));
+        System.out.println(Arrays.toString(s1.solution(arr1,arr3)));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,21 @@
 import java.util.Arrays;
 
 class Solution {
-    public int solution(int[] sides) {
-        int answer = 0;
-        Arrays.sort(sides);
-        int min = sides[0];
-        int max = sides[1];
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        Arrays.fill(answer, -1);
 
-        for(int i = max - min + 1 ; i <= max ; i++){
-            answer++;
+        for (int idx = 0; idx < queries.length; idx++) {
+            int[] query = queries[idx];
+            int s = query[0], e = query[1], k = query[2];
+
+            for (int i = s; i <= e; i++) {
+                if (k < arr[i]) {
+                    answer[idx] = answer[idx] == -1 ? arr[i] : Math.min(answer[idx], arr[i]);
+                }
+            }
         }
 
-        for(int i = max + 1 ; i < min + max ; i++){
-            answer++;
-        }
         return answer;
     }
 }
