b52e150ba99f683d931d47c857ff3d701fd6fb69
kinam <maltize9292@gmail.com>
2025-03-23 00:03:39 +0900

    프로그래머스

--- src/Solution.java
+++ src/Solution.java
@@ -1,16 +1,12 @@
 class Solution {
-    public String[] solution(String[] quiz) {
-		String[] answer = new String[quiz.length];
-		for (int i = 0; i < quiz.length; i++) {
+    public int solution(int num, int k) {
+        String s = String.valueOf(num);
 
-			String arr[] = quiz[i].split(" ");
-
-			int left = arr[1].equals("+") ? Integer.parseInt(arr[0]) + Integer.parseInt(arr[2])
-					: Integer.parseInt(arr[0]) - Integer.parseInt(arr[2]);
-			int right = Integer.parseInt(arr[4]);
-
-			answer[i] = left == right ? "O" : "X";
-		}
-		return answer;
-	}
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) - '0' == k){
+                return i + 1;
+            }
+        }
+        return -1;
+    }
 }
