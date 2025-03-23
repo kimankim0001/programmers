88ffebc52bf58911e2e268a39ba0c6dc89c02483
kinam <maltize9292@gmail.com>
2025-03-24 00:14:12 +0900

    프로그래머스

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,14 @@ public class Main {
         Solution s1 = new Solution();
 
         int[] arr1 = {1, 2, 7, 10, 11};
-        int[][] arr3 = {{0, 4, 2},{0, 3, 2},{0, 2, 2}};
+        int[][] arr3 = {{1, 4},{9, 2},{3, 8},{11, 6}};
         
         boolean[] boo1 = {false, true, true, true, true, false, false};
         
         String[] str1 = {"3 - 4 = -3", "5 + 6 = 11"};
         String[][] str2 = {{"rardss", "123"},{"yyoom", "1234"},{"meosseugi", "1234"}};
                                
-        System.out.println(s1.solution(29183,1));
+        System.out.println(s1.solution(arr3));
 //        System.out.println(s1.solution("hello", "ohell"));
 //        System.out.println(Arrays.toString(s1.solution(str1)));
     }
