5ab2b2c7e33f85245f9b1ad27a27aa55b4b2d074
kinam <maltize9292@gmail.com>
2025-04-02 18:33:43 +0900

    프로그래머스

--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,14 @@
-import java.util.Arrays;
-
 class Solution {
-    public int[][] solution(int[] num_list, int n) {
-        int[][] answer = new int[num_list.length / n][n];
-        int count = 0;
-        for (int i = 0; i < answer.length; i++) {
-            for (int j = 0; j < answer[i].length; j++, count++) {
-                answer[i][j] = num_list[count];
-            }
-        }
+    public int solution(int[] dot) {
+        int answer = 0;
+        if(dot[0] > 0) 
+            if(dot[1] > 0) answer = 1;
+            else answer = 4;
+        else 
+            if(dot[1] > 0) answer = 2;
+            else answer = 3;
+
+
         return answer;
     }
 }
