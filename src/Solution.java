6a96e872daf148e5f5c022b8a77ca19be7aede6a
kinam <maltize9292@gmail.com>
2025-04-05 07:47:50 +0900

    프로그래머스

--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,45 @@
 class Solution {
-    public int solution(int slice, int n) {
+    public int solution(int n, int w, int num) {
         int answer = 0;
-        if(n % slice == 0)
-            answer = n / slice;
-        else 
-            answer = n / slice + 1;
-        return answer;
+
+        int []width_num = new int[w];
+        for(int i=0; i<w;i++)
+        {
+            width_num[i]=n/w;
+        }
+        
+        if(n/w%2!=0) 
+        {
+            for(int i=w-1;i>w-1-n%w;i--)
+            {
+                width_num[i]+=1;
+            }
+        }
+        
+        else if(n/w%2==0) 
+        {
+            for(int i=0;i<n%w;i++)
+            {
+                width_num[i]+=1;
+            }
+        }
+    
+        if(num%w==0 && num/w%2==0){  
+            
+            answer = width_num[0]-num/w+1;
+        }
+        else if(num%w==0 && num/w%2!=0){ 
+           
+            answer = width_num[w-1]-num/w+1;
+        }
+        
+        if(num/w%2==0 && num%w!=0){
+            
+            answer = width_num[num%w-1]-num/w;
+        }
+        else if(num/w%2!=0 && num%w!=0){
+            answer = width_num[w-num%w]-num/w;
+        }
+        return answer; 
     }
 }
