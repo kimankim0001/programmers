8b80e6d76aac6d6c8d5428588f79c42c8ba83696
kinam <maltize9292@gmail.com>
2025-04-12 01:00:14 +0900

    programmers

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         Solution s1 = new Solution();
 
-        int[] arr1 = {5,1,3,7};
+        int[] arr1 = {4, 11};
         int[] arr11 = {2,2,6,8};
         int[][] arr2 = {{80,70},{90,50},{40,70},{50,80}};
         
@@ -15,7 +15,7 @@ public class Main {
         String[] str1 = {"BACDE", "CBADF", "AECB", "BDA"};
         String[][] str2 = {{"rardss", "123"},{"yyoom", "1234"},{"meosseugi", "1234"}};
                                
-        System.out.println(s1.solution(arr1,arr11));
+        System.out.println(s1.solution(11, arr1, 1));
 //        System.out.println(Arrays.toString(s1.solution(3,str1)));
 //        System.out.println(Arrays.deepToString(s1.solution(arr1, 2)));
     }
--- src/Solution.java
+++ src/Solution.java
@@ -1,30 +1,19 @@
-import java.util.*;
-
 class Solution {
-    public int solution(int[] A, int[] B) {
-    	// 점수 역순으로 pq에 넣는다.
-        PriorityQueue<Integer> pq = new PriorityQueue(Collections.reverseOrder());
-        for(int i : A)
-            pq.add(i);
-        
-        // 점수순으로 정렬하여 Deque에 삽입
-        Arrays.sort(B);
-        ArrayDeque<Integer> dq = new ArrayDeque();
-        for(int i : B)
-            dq.add(i);
-        
+    public int bsearch(int begin, int end, int w) {
+        int res = (end - begin + 1) / (2 * w + 1);
+        if((end - begin + 1) % (2 * w + 1) > 0) res++;
+        return res;
+    }
+    public int solution(int n, int[] stations, int w) {
         int answer = 0;
-        while(!pq.isEmpty()){
-            int target = pq.poll();
-            // B팀의 최고 숫자가 A팀의 최고 숫자보다 높은 경우 승리
-            if(target < dq.peekLast()){
-                dq.pollLast();
-                answer++;
-            }else{ // 질때는 가장 작은 숫자를 내며 져야 한다.
-                dq.pollFirst();
-            }
+        int begin = 1;
+        for(int i = 0; i < stations.length; i++) {
+            if(begin < stations[i] - w)
+                answer += bsearch(begin, stations[i] - w - 1, w);
+            begin = stations[i] + w + 1;
         }
-        
+        if(stations[stations.length - 1] + w < n)
+            answer += bsearch(stations[stations.length - 1] + w + 1, n, w);
         return answer;
     }
 }
