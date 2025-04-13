47c7a10a2b4232079f1f1976783326908122285e
kinam <maltize9292@gmail.com>
2025-04-14 00:29:54 +0900

    programmers

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,15 @@ public class Main {
 
         int[] arr1 = {9, 20, 28, 18, 11};
         int[] arr2 = {30, 1, 21, 17, 28};
-        int[][] arr11 = {{80,70},{90,50},{40,70},{50,80}};
+        int[][] arr11 = {{1,2,1},{2,3,3},{5,2,2},{1,4,2},{5,3,1},{5,4,2}};
         
         boolean[] boo1 = {false, true, true, true, true, false, false};
         
         String[] str1 = {"BACDE", "CBADF", "AECB", "BDA"};
         String[][] str2 = {{"rardss", "123"},{"yyoom", "1234"},{"meosseugi", "1234"}};
                                
-//        System.out.println(s1.solution(5, arr1, arr2));
+        System.out.println(s1.solution(5,arr11,3));
 //        System.out.println(Arrays.toString(s1.solution(3,str1)));
-        System.out.println(Arrays.deepToString(s1.solution(5, arr1, arr2)));
+//        System.out.println(Arrays.deepToString(s1.solution(5, arr1, arr2)));
     }
 }
