fb37f28120e1715198cb5b8aaf8a77d8739cbe12
kinam <maltize9292@gmail.com>
2025-04-17 00:09:40 +0900

    programmers

--- src/Solution.java
+++ src/Solution.java
@@ -1,21 +1,6 @@
 class Solution {
-    public long solution(int w, int h) {
-        long answer = 1;
-        long big = gcd(w,h);
-        long weight = (long)w;
-        long height = (long)h;
-        
-        answer = weight*height - ((weight/big)+(height/big)-1)*big;
-        
-        return answer;
-    }
-    
-    public static long gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return (long)a;
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        return answer = num1 + num2;
     }
 }
