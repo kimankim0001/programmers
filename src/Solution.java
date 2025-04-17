0a13d5607f58a57d9d3c14b5f00a0cc676da4086
kinam <maltize9292@gmail.com>
2025-04-18 00:57:12 +0900

    programmers

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,8 @@
 class Solution {
-    public int solution(int num1, int num2) {
-        int answer = -1;
-        return answer = num1 + num2;
+
+    public int solution(int n, int k) {
+        int answer = 0;
+        answer = (n * 12000) + ((k - (n / 10)) * 2000);
+        return answer;
     }
 }
