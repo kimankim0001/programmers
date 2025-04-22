fb61daa375afe678ca373474b214d98367e1f303
kinam <maltize9292@gmail.com>
2025-04-23 00:40:56 +0900

    pro

--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,11 @@
 class Solution {
 
-    public int solution(int[] array, int n) {
-        int answer = 0;
+    public int[] solution(int[] array) {
+        int[] answer = {0, 0};
         for (int i = 0; i < array.length; i++) {
-            if (array[i] == n) {
-                answer++;
+            if (array[i] > answer[0]) {
+                answer[0] = array[i];
+                answer[1] = i;
             }
         }
         return answer;
