145315a2730d02d5e605148f3324ce8c0cb0c29c
kinam <maltize9292@gmail.com>
2025-04-25 00:00:57 +0900

    pro

--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,13 @@
 class Solution {
-    public String solution(String myString) {
-        String answer = "";
-        int tmp;
-        for(int i=0; i<myString.length(); i++){
-            tmp = (int)myString.charAt(i);
-            if((65<=tmp)&&(tmp<=90)){
-                answer += (char)(tmp+32);
-            }else{
-                answer += (char)tmp;
-            }
+
+    public int solution(String number) {
+        int answer = 0;
+        int sum = 0;
+        String[] number_list = number.split("");
+        for (int i = 0; i < number_list.length; i++) {
+            sum += Integer.parseInt(number_list[i]);
         }
+        answer = sum % 9;
         return answer;
     }
 }
