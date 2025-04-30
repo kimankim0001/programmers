a894f6f314bd0e7b771cd78e5754eb644312c69a
kinam <maltize9292@gmail.com>
2025-04-30 18:31:50 +0900

    pro

--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,10 @@
 class Solution {
-    public String solution(int[] numLog) {
-		String answer = "";
-		for (int i = 1; i < numLog.length; i++) {
-                 if((numLog[i] - numLog[i-1]) == 1) answer += "w";
-			else if((numLog[i] - numLog[i-1]) == -1) answer += "s";
-			else if((numLog[i] - numLog[i-1]) == 10) answer += "d";
-			else if((numLog[i] - numLog[i-1]) == -10) answer += "a";
-		}
-		return answer;
-	}
+
+    public int solution(int[] box, int n) {
+        int a = box[0] / n;
+        int b = box[1] / n;
+        int c = box[2] / n;
+        int answer = a * b * c;
+        return answer;
+    }
 }
