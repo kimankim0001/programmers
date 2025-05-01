ab8d403b633b45112492d0e27e17471f2a59f101
kinam <maltize9292@gmail.com>
2025-05-02 07:39:11 +0900

    pro

--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,10 @@
 class Solution {
-    public int solution(int[] dot) {
-        int answer = 0;
-        if(dot[0] > 0) 
-            if(dot[1] > 0) answer = 1;
-            else answer = 4;
-        else 
-            if(dot[1] > 0) answer = 2;
-            else answer = 3;
-
-
-        return answer;
+    public int[] solution(int money) {
+  	int[] answer = new int[2];
+    
+    answer[0] = money / 5500;
+    answer[1] = money % 5500;
+    
+    return answer;
     }
-}
+}  
