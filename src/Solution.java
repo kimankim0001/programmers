a2d0a8fe7fc4b47f6671f448ba7867dda7a39bca
kinam <maltize9292@gmail.com>
2025-05-08 20:14:46 +0900

    pro

--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,9 @@
 class Solution {
-    public String solution(String myString) {
-        String answer = "";
-        int tmp;
-        for(int i=0; i<myString.length(); i++){
-            tmp = (int)myString.charAt(i);
-            if((65<=tmp)&&(tmp<=90)){
-                answer += (char)(tmp+32);
-            }else{
-                answer += (char)tmp;
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 0; i <= n ; i++) {
+            if (i%2 == 0){
+                answer += i;
             }
         }
         return answer;
