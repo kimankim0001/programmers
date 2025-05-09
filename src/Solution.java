b4b1f0d2ba2a66ab18bf8eed4181810708934a56
kinam <maltize9292@gmail.com>
2025-05-10 00:23:09 +0900

    pro

--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,10 @@
 class Solution {
-
-    public String solution(String my_string, String letter) {
-        String answer = "";
-
-        answer = my_string.replace(letter, "");
-        
-        return answer;
+    public int[] solution(int money) {
+  	int[] answer = new int[2];
+    
+    answer[0] = money / 5500;
+    answer[1] = money % 5500;
+    
+    return answer;
     }
-}
+}  
