d41a259147183d24b64b2464ba6eff98ff1d86e5
kinam <maltize9292@gmail.com>
2025-05-20 18:31:37 +0900

    pro

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,8 @@
 class Solution {
-    public int solution(int age) {
-        int answer = 2022-age+1;
+
+    public int solution(int n, int k) {
+        int answer = 0;
+        answer = (n * 12000) + ((k - (n / 10)) * 2000);
         return answer;
     }
 }
