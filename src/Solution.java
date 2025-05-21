cc427dbea0620df2fd585576e4779ffe64265c6b
kinam <maltize9292@gmail.com>
2025-05-22 00:03:10 +0900

    pro

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,10 @@
 class Solution {
 
-    public int[] solution(int[] num_list) {
-        int[] answer = new int[2];
-        int left = 0;
-        int right = 0;
+    public String solution(String my_string, String letter) {
+        String answer = "";
 
-        for (int i = 0; i < num_list.length; i++) {
-            if (num_list[i] % 2 == 0) {
-                left++;
-            } else {
-                right++;
-            }
-        }
-        answer[0] = left;
-        answer[1] = right;
+        answer = my_string.replace(letter, "");
+        
         return answer;
     }
 }
