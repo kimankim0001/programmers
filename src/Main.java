63ed802caa01bb9d76b128d87ba81816cc61322a
kinam <maltize9292@gmail.com>
2025-05-21 18:19:16 +0900

    pro

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         Solution s1 = new Solution();
 
-        int[] arr1 = {2,4};
+        int[] arr1 = {1, 2, 3, 4, 5};
         int[] arr2 = {30, 1, 21, 17, 28};
         int[][] arr11 = {{1,2,1},{2,3,3},{5,2,2},{1,4,2},{5,3,1},{5,4,2}};
         
@@ -15,8 +15,8 @@ public class Main {
         String[] str1 = {"and","notad","abcd"};
         String[][] str2 = {{"rardss", "123"},{"yyoom", "1234"},{"meosseugi", "1234"}};
                                
-        System.out.println(s1.solution(10,3));
-//        System.out.println(Arrays.toString(s1.solution(5500)));
+//        System.out.println(s1.solution(10,3));
+        System.out.println(Arrays.toString(s1.solution(arr1)));
 //        System.out.println(Arrays.deepToString(s1.solution(5, arr1, arr2)));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,8 +1,19 @@
 class Solution {
 
-    public int solution(int n, int k) {
-        int answer = 0;
-        answer = (n * 12000) + ((k - (n / 10)) * 2000);
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        int left = 0;
+        int right = 0;
+
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] % 2 == 0) {
+                left++;
+            } else {
+                right++;
+            }
+        }
+        answer[0] = left;
+        answer[1] = right;
         return answer;
     }
 }
