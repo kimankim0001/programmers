1c497d282147945eeb8667238fad127f14d37f13
kinam <maltize9292@gmail.com>
2025-05-24 01:09:24 +0900

    pro

--- src/Solution.java
+++ src/Solution.java
@@ -1,9 +1,19 @@
 class Solution {
 
-    public int solution(String message) {
-        int answer = 0;
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        int left = 0;
+        int right = 0;
 
-        answer = message.length() * 2;
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] % 2 == 0) {
+                left++;
+            } else {
+                right++;
+            }
+        }
+        answer[0] = left;
+        answer[1] = right;
         return answer;
     }
 }
