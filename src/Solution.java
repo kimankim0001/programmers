0812552092d5a741a020d0e18a55725f2dae078b
kinam <maltize9292@gmail.com>
2025-05-27 22:34:34 +0900

    pro

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,9 @@
 class Solution {
-
-    public int[] solution(int n, int[] numlist) {
-        int count = 0;
-
-        for (int i = 0; i < numlist.length; i++) {
-            if (numlist[i] % n == 0) {
-                count++;
-            }
+    public double solution(int[] numbers) {
+        double answer = 0;
+        for (int i = 0 ; i < numbers.length; i++) {
+            answer += (double)numbers[i];
         }
-        int[] answer = new int[count];
-
-        int index = 0;
-        for (int i = 0; i < numlist.length; i++) {
-            if (numlist[i] % n == 0) {
-                answer[index] = numlist[i];
-                index++;
-            }
-        }
-        return answer;
+        return answer/numbers.length;
     }
 }
